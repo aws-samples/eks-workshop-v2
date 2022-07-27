e3e09a546ab97838af644002c21b0d844696c446
Niall Thomson <thomson.niall@gmail.com>
2022-07-27 11:10:21 -0600

    Updated destroy infrastructure script to reflect EKS blueprints suggested cleanup procedure

--- hack/destroy-infrastructure.sh
+++ hack/destroy-infrastructure.sh
@@ -8,4 +8,12 @@ PROJECT_ROOT="$SCRIPT_DIR/.."
 
 TERRAFORM_DIR="$PROJECT_ROOT/terraform/local"
 
+# Deletion procedure reflects recommendations from EKS Blueprints: 
+# https://aws-ia.github.io/terraform-aws-eks-blueprints/v4.6.0/getting-started/#cleanup
+
+terraform -chdir=$TERRAFORM_DIR destroy -target=module.cluster.module.eks-blueprints-kubernetes-addons --auto-approve
+terraform -chdir=$TERRAFORM_DIR destroy -target=module.cluster.module.descheduler --auto-approve
+
+terraform -chdir=$TERRAFORM_DIR destroy -target=module.cluster.module.aws-eks-accelerator-for-terraform --auto-approve
+
 terraform -chdir=$TERRAFORM_DIR destroy --auto-approve
