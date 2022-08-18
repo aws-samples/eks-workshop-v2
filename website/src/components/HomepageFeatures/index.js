22fc07bc0107ec3a67222914f0c23577d21c9f5c
Niall Thomson <thomson.niall@gmail.com>
2022-08-18 10:15:48 -0600

    Added placeholder callout boxes to index page

--- website/src/components/HomepageFeatures/index.js
+++ website/src/components/HomepageFeatures/index.js
@@ -4,32 +4,29 @@ import styles from './styles.module.css';
 
 const FeatureList = [
   {
-    title: 'Easy to Use',
-    Svg: require('@site/static/img/undraw_docusaurus_mountain.svg').default,
+    title: 'Accelerated path',
+    Svg: require('@site/static/img/accelerate.svg').default,
     description: (
       <>
-        Docusaurus was designed from the ground up to be easily installed and
-        used to get your website up and running quickly.
+        Navigate through the features of Amazon Elastic Kubernetes Services quickly.
       </>
     ),
   },
   {
-    title: 'Focus on What Matters',
-    Svg: require('@site/static/img/undraw_docusaurus_tree.svg').default,
+    title: 'Self-Paced',
+    Svg: require('@site/static/img/self-paced.svg').default,
     description: (
       <>
-        Docusaurus lets you focus on your docs, and we&apos;ll do the chores. Go
-        ahead and move your docs into the <code>docs</code> directory.
+        Learn at your own pace using practical examples.
       </>
     ),
   },
   {
-    title: 'Powered by React',
-    Svg: require('@site/static/img/undraw_docusaurus_react.svg').default,
+    title: 'Modular',
+    Svg: require('@site/static/img/modules.svg').default,
     description: (
       <>
-        Extend or customize your website layout by reusing React. Docusaurus can
-        be extended while reusing the same header and footer.
+        Customize your learning path by focusing on the features that matter most to you.
       </>
     ),
   },
