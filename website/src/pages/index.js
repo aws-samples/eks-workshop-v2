06296bac237b1da308cb589fc6fc81b5f179d0e4
Niall Thomson <thomson.niall@gmail.com>
2022-08-18 09:40:48 -0600

    Fix hero element background color and content

--- website/src/pages/index.js
+++ website/src/pages/index.js
@@ -18,7 +18,7 @@ function HomepageHeader() {
           <Link
             className="button button--secondary button--lg"
             to="/docs/introduction">
-            Docusaurus Tutorial - 5min ⏱️
+            Get Started!
           </Link>
         </div>
       </div>
