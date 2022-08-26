aaf29f51bb3c43586e57d48ebc1eb3bb1065f7f9
Henrique Santana <cssntan@amazon.com>
2022-08-26 14:48:31 +0000

    Changed the docker binary path to be relative. This would avoid issues with different OS

--- hack/markdownlint.sh
+++ hack/markdownlint.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-/usr/bin/docker run -v $PWD:/workdir ghcr.io/igorshubovych/markdownlint-cli:latest "website/docs/**/*.md" 
+docker run -v $PWD:/workdir ghcr.io/igorshubovych/markdownlint-cli:latest "website/docs/**/*.md" 
