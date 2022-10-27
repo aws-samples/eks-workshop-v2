29a875f036eb337a45a96a6cbd869884648bcef5
Masatoshi Hayashi <m@literalice.com>
2022-10-27 17:29:42 +0900

    adds a section for AI/ML

---
+++ website/docs/aiml/index.md
@@ -0,0 +1,8 @@
+---
+title: "AI/ML"
+sidebar_position: 6
+weight: 40
+---
+
+Overview of AI/ML workloads on EKS
+Discuss how to manage MLOps pipelines or use AWS Inferentia and AWS Trainium.
--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -116,6 +116,12 @@ const config = {
             label: 'GitOps',
           },
           {
+            type: 'doc',
+            docId: 'aiml/index',
+            position: 'left',
+            label: 'AI/ML',
+          },
+          {
             href: 'https://github.com/aws-samples/eks-workshop-v2',
             position: 'right',
             className: 'header-github-link',
