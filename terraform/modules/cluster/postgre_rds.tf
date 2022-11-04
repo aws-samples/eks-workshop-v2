1f899d60c9e976488442fa2745836b049c5dc0f6
Umair Ishaq <umair29@gmail.com>
2022-11-03 23:35:57 -0400

    Update DB version; minor tweaks

--- terraform/modules/cluster/postgre_rds.tf
+++ terraform/modules/cluster/postgre_rds.tf
@@ -3,8 +3,8 @@ module "networking_rds_postgre" {
 
   name           = "eksworkshop-rds-networking"
   engine         = "aurora-postgresql"
-  engine_version = "11.12"
-  instance_class = "db.t3.small"
+  engine_version = "11.13"
+  instance_class = "db.t3.medium"
   instances = {
     one = {}
   }
@@ -12,21 +12,17 @@ module "networking_rds_postgre" {
   vpc_id  = module.aws_vpc.vpc_id
   subnets = local.private_subnet_ids
 
-  create_security_group = false
-  allowed_security_groups = [aws_security_group.networking_rds_ingress.id]
-
-  master_username = "eksworkshop"
-  master_password        = random_string.networking_db_master.result
-  create_random_password = false
-  database_name          = "eksworkshop"
-  storage_encrypted      = true
-  apply_immediately      = true
-  skip_final_snapshot    = true
-  backup_retention_period = 0
-  monitoring_interval = 10
-
+  allowed_security_groups  = [aws_security_group.networking_rds_ingress.id]
+  master_username         = "eksworkshop"
+  master_password         = random_string.networking_db_master.result
+  create_random_password  = false
+  database_name           = "eksworkshop"
+  storage_encrypted       = true
+  apply_immediately       = true
+  skip_final_snapshot     = true
+  backup_retention_period = 1
+  monitoring_interval     = 10
   iam_database_authentication_enabled = true
-
   enabled_cloudwatch_logs_exports = ["postgresql"]
 
   tags = local.tags
@@ -38,6 +34,7 @@ resource "random_string" "networking_db_master" {
 }
 
 resource "aws_security_group" "networking_rds_ingress" {
+  name        = "${local.cluster_name}-networking-db"
   description = "Allow inbound traffic to security group networking module RDS"
   vpc_id      = module.aws_vpc.vpc_id
 
