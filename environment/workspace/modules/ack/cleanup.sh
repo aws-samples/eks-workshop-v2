44a4c6e18732b2384a07489c6f827e85ed9fd4ff
Carlos Santana <csantana23@gmail.com>
2022-11-09 12:32:02 -0500

    more precise cleanup ack
    Signed-off-by: Carlos Santana <csantana23@gmail.com>

--- environment/workspace/modules/ack/cleanup.sh
+++ environment/workspace/modules/ack/cleanup.sh
@@ -3,19 +3,19 @@
 set -oux pipefail
 
 kubectl delete -k /workspace/modules/ack/manifests/
-kubectl delete -n default fieldexports.services.k8s.aws --all
-kubectl delete -n default brokers.mq.services.k8s.aws --all
-kubectl delete -n default dbinstances.rds.services.k8s.aws --all
-kubectl delete -n default dbsubnetgroups.rds.services.k8s.aws --all
-kubectl delete -n default securitygroup.ec2.services.k8s.aws --all
+kubectl delete -k /workspace/modules/ack/mq/fieldexports
+kubectl delete -k /workspace/modules/ack/rds/fieldexports
+kubectl delete -k /workspace/modules/ack/mq/k8s/broker
+kubectl delete -k /workspace/modules/ack/mq/k8s/security-group
+kubectl delete -k /workspace/modules/ack/rds/k8s
 kubectl delete -n default secret rds-eks-workshop
 kubectl delete -n default secret mq-eks-workshop
-kubectl delete -n ack-system roles.iam.services.k8s.aws  --all
-kubectl delete -n ack-system policies.iam.services.k8s.aws --all
+kubectl delete -k /workspace/modules/ack/ec2
+kubectl delete -k /workspace/modules/ack/rds/roles
 helm uninstall -n ack-system ack-rds-controller
 helm uninstall -n ack-system ack-mq-controller
 helm uninstall -n ack-system ack-ec2-controller
 helm uninstall -n ack-system ack-iam-controller
+kubectl delete namespace ack-system
 aws iam delete-role-policy --role-name ack-iam-controller --policy-name ack-iam-recommended-policy
 aws iam delete-role --role-name ack-iam-controller
-exit 0
