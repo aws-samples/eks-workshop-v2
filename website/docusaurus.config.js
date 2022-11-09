bdedf441b8e7634ddd4bd759d59148731bfeb764
Niall Thomson <thomson.niall@gmail.com>
2022-11-09 23:09:44 +0000

    Remove color mode button

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -60,6 +60,9 @@ const config = {
   themeConfig:
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
+      colorMode: {
+        disableSwitch: true,
+      },
       navbar: {
         title: 'EKS Workshop',
         logo: {
