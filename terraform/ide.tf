2d5c4d7b2c59282023291507f0e868647514a0a8
Niall Thomson <thomson.niall@gmail.com>
2022-11-18 05:33:37 +0000

    Turn off AWS CLI pager default

--- terraform/ide.tf
+++ terraform/ide.tf
@@ -91,6 +91,8 @@ if [[ ! -d "/home/ec2-user/.bashrc.d" ]]; then
   sudo -H -u ec2-user bash -c "echo 'for file in ~/.bashrc.d/*.bash; do source \"\$file\"; done' >> ~/.bashrc"
 fi
 
+sudo -H -u ec2-user bash -c "echo 'export AWS_PAGER=\"\"' > ~/.bashrc.d/aws.bash"
+
 sudo -H -u ec2-user bash -c "echo 'aws eks update-kubeconfig --name ${module.cluster.eks_cluster_id}' > ~/.bashrc.d/kubeconfig.bash"
 
 cat << EOT > /home/ec2-user/.bashrc.d/env.bash
