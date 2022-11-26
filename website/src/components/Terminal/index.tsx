a6be7c8512a23bef5c151dbd95a6994e10394ba5
Niall Thomson <thomson.niall@gmail.com>
2022-11-26 04:01:01 +0000

    Change terminal view formatting

--- website/src/components/Terminal/index.tsx
+++ website/src/components/Terminal/index.tsx
@@ -36,10 +36,10 @@ export default function Terminal({
         section = new TerminalSection()
         sections.push(section)
 
-        currentLine = currentLine.substring(2).trim()
+        currentLine = currentLine.substring(2)
       }
 
-      if(section.processLine(currentLine)) {
+      if(section.processLine(currentLine.trim())) {
         allCommands = `${allCommands}\n${currentLine}`
       }
     }
@@ -175,21 +175,22 @@ class TerminalCommand extends TerminalContext {
     return (
       <div>
         <div className={styles.terminalPrompt}>
-        <span className={styles.terminalPromptUser}>eks-workshop:</span>
         <span className={styles.terminalPromptLocation}>~</span>
         <span className={styles.terminalPromptBling}>$</span>
-        {this.renderCommand(this.lines[0])}
+        {this.renderCommand(this.lines[0], false)}
       </div>
       { this.lines.slice(1).map(element => {
-        return (<div className={styles.terminalPrompt}>{this.renderCommand(element)}</div>)
+        return (<div className={styles.terminalPrompt}>{this.renderCommand(element, true)}</div>)
       })
       }
       </div>
     )
   }
 
-  renderCommand(command: string) {
-    return (<span className={styles.terminalPromptCommand}>{command}</span>)
+  renderCommand(command: string, indent: boolean) {
+    let output = indent ? `     ${command}` : command;
+
+    return (<span className={styles.terminalPromptCommand}>{output}</span>)
   }
 }
 
