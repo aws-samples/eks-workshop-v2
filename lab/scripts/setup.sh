f2b9b02f1f0d382dfd778d6eb9a02415e2b12a29
Niall Thomson <thomson.niall@gmail.com>
2023-03-31 00:07:06 +0000

    Add script pointers if ref set

--- lab/scripts/setup.sh
+++ lab/scripts/setup.sh
@@ -10,14 +10,35 @@ if [[ ! -d "~/.bashrc.d" ]]; then
   echo 'for file in ~/.bashrc.d/*.bash; do source "$file"; done' >> ~/.bashrc
 fi
 
-#echo 'aws cloud9 update-environment --environment-id $CLOUD9_ENVIRONMENT_ID --managed-credentials-action DISABLE &> /dev/null || true' > ~/.bashrc.d/c9.bash
+if [ ! -z "$CLOUD9_ENVIRONMENT_ID" ]; then
+  echo 'aws cloud9 update-environment --environment-id $CLOUD9_ENVIRONMENT_ID --managed-credentials-action DISABLE &> /dev/null || true' > ~/.bashrc.d/c9.bash
+fi
 
 echo 'export AWS_PAGER=""' > ~/.bashrc.d/aws.bash
-
-#echo 'aws eks update-kubeconfig --name ${module.cluster.eks_cluster_id} > /dev/null' > ~/.bashrc.d/kubeconfig.bash
 
 touch ~/.bashrc.d/workshop-env.bash
 
 cat << EOT > /home/ec2-user/.bashrc.d/aliases.bash
 function prepare-environment() { bash /usr/local/bin/reset-environment \$1; source ~/.bashrc.d/workshop-env.bash; }
 EOT
+
+if [ ! -z "$REPOSITORY_REF" ]; then
+  cat << EOT > /usr/local/bin/reset-environment
+#!/bin/bash
+set -e
+curl -fsSL https://raw.githubusercontent.com/aws-samples/eks-workshop-v2/\$REPOSITORY_REF/lab/bin/reset-environment | bash
+EOT
+  chmod +x /usr/local/bin/reset-environment
+  cat << EOT > /usr/local/bin/delete-environment
+#!/bin/bash
+set -e
+curl -fsSL https://raw.githubusercontent.com/aws-samples/eks-workshop-v2/\$MANIFESTS_REF/lab/bin/delete-environment | bash
+EOT
+  chmod +x /usr/local/bin/delete-environment
+  cat << EOT > /usr/local/bin/wait-for-lb
+#!/bin/bash
+set -e
+curl -fsSL https://raw.githubusercontent.com/aws-samples/eks-workshop-v2/\$MANIFESTS_REF/lab/bin/wait-for-lb | bash -s -- \$1
+EOT
+  chmod +x /usr/local/bin/wait-for-lb
+fi
