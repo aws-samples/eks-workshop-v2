4cd52cc1b24b3f6e06a58e58c7a60feab3d07b73
Niall Thomson <thomson.niall@gmail.com>
2023-04-06 19:49:02 +0000

    Tweak output of use-cluster

--- lab/scripts/setup.sh
+++ lab/scripts/setup.sh
@@ -20,6 +20,8 @@ touch ~/.bashrc.d/workshop-env.bash
 
 cat << EOT > /home/ec2-user/.bashrc.d/aliases.bash
 function prepare-environment() { bash /usr/local/bin/reset-environment \$1; source ~/.bashrc.d/workshop-env.bash; }
+
+function use-cluster() { bash /usr/local/bin/use-cluster \$1; source ~/.bashrc.d/env.bash; }
 EOT
 
 if [ ! -z "$REPOSITORY_REF" ]; then
