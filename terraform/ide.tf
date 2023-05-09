816962da6fccad679ee87bb8555a8688d483671e
Benjamin Gardiner <gardineb@amazon.com>
2023-05-09 10:29:09 -0700

    Changed variable name to match others

--- terraform/ide.tf
+++ terraform/ide.tf
@@ -41,7 +41,7 @@ VPC_PRIVATE_SUBNET_ID_2=${try(module.cluster.private_subnet_ids[2], "")}
 AIML_DL_IMAGE=763104351884.dkr.ecr.${data.aws_region.current.name}.amazonaws.com/pytorch-inference-neuron:1.12.1-neuron-py38-sdk2.5.0-ubuntu20.04
 AIML_NEURON_BUCKET_NAME=${try(module.cluster.aiml_neuron_s3_bucket_name, "")}
 AIML_NEURON_ROLE_ARN=${try(module.cluster.aiml_neuron_role_arn, "")}
-AI_ML_SUBNET=${module.cluster.private_subnet_ids[0]},${module.cluster.private_subnet_ids[1]},${module.cluster.private_subnet_ids[2]}
+AIML_SUBNET=${module.cluster.private_subnet_ids[0]},${module.cluster.private_subnet_ids[1]},${module.cluster.private_subnet_ids[2]}
 
 GITOPS_IAM_SSH_KEY_ID=${try(module.cluster.gitops_iam_ssh_key_id, "")}
 GITOPS_IAM_SSH_USER=${try(module.cluster.gitops_ssh_iam_user, "")}
