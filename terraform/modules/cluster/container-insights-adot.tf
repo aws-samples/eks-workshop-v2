c01a17b0a4eb2537bd2a466a98d5cf91208160cc
Niall Thomson <thomson.niall@gmail.com>
2023-05-23 22:50:40 +0000

    Ensure Grafana and custom dashboards are installed in the correct order

--- terraform/modules/cluster/container-insights-adot.tf
+++ terraform/modules/cluster/container-insights-adot.tf
@@ -9,3 +9,63 @@ module "iam_assumable_role_adot_ci" {
 
   tags = local.tags
 }
+
+resource "kubernetes_namespace" "grafana" {
+  metadata {
+    name = "grafana"
+  }
+}
+
+module "eks_blueprints_kubernetes_grafana_addon" {
+  source = "github.com/aws-ia/terraform-aws-eks-blueprints?ref=v4.16.0//modules/kubernetes-addons/grafana"
+
+  depends_on = [
+    module.eks_blueprints_kubernetes_addons,
+    kubernetes_config_map.order-service-metrics-dashboard
+  ]
+
+  addon_context = local.addon_context
+
+  irsa_policies = [
+    aws_iam_policy.grafana.arn
+  ]
+
+  helm_config = {
+    create_namespace = false
+    namespace        = kubernetes_namespace.grafana.metadata[0].name
+
+    values = [templatefile("${path.module}/templates/grafana.yaml", { prometheus_endpoint = aws_prometheus_workspace.this.prometheus_endpoint, region = data.aws_region.current.name })]
+  }
+}
+
+resource "kubernetes_config_map" "order-service-metrics-dashboard" {
+  metadata {
+    name      = "order-service-metrics-dashboard"
+    namespace = kubernetes_namespace.grafana.metadata[0].name
+
+    labels = {
+      grafana_dashboard = 1
+    }
+  }
+  
+  data = {
+    "order-service-metrics-dashboard.json" = file("${path.module}/templates/dashboards/order-service-metrics-dashboard.json")
+  }
+}
+
+resource "aws_iam_policy" "grafana" {
+  name = "${var.environment_name}-grafana-other"
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = [
+          "aps:*",
+        ]
+        Effect   = "Allow"
+        Resource = "*"
+      },
+    ]
+  })
+}
