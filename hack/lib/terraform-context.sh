6ee21013b076bf116208a40d1bae18e59a9855b0
Carlos Santana <csantana23@gmail.com>
2023-06-05 08:54:10 -0400

    Enable make shell to work with finch in addition of docker Fixes #539
    Signed-off-by: Carlos Santana <csantana23@gmail.com>

--- hack/lib/terraform-context.sh
+++ hack/lib/terraform-context.sh
@@ -2,6 +2,4 @@ terraform_dir="$SCRIPT_DIR/../$terraform_context"
 
 export ASSUME_ROLE=$(terraform -chdir=$terraform_dir output -raw iam_role_arn)
 
-TEMP='/tmp/eks-workshop-shell-env'
-
-terraform -chdir=$terraform_dir output -raw environment_variables > $TEMP
+terraform -chdir=$terraform_dir output -raw environment_variables > ${TEMP:-/tmp}/eks-workshop-shell-env
--- hack/run-tests.sh
+++ hack/run-tests.sh
@@ -5,6 +5,10 @@ module=$2
 
 set -Eeuo pipefail
 set -u
+
+# You can run script with finch like CONTAINER_CLI=finch ./run-tests.sh
+CONTAINER_CLI=${CONTAINER_CLI:-docker}
+
 # Right now the container images are only designed for amd64
 export DOCKER_DEFAULT_PLATFORM=linux/amd64
 
@@ -23,16 +27,16 @@ echo "Building container images..."
 
 container_image='eks-workshop-test'
 
-(cd $SCRIPT_DIR/../environment && docker build -q -t eks-workshop-environment .)
+(cd $SCRIPT_DIR/../environment && $CONTAINER_CLI build -q -t eks-workshop-environment .)
 
-(cd $SCRIPT_DIR/../test && docker build -q -t $container_image .)
+(cd $SCRIPT_DIR/../test && $CONTAINER_CLI build -q -t $container_image .)
 
 source $SCRIPT_DIR/lib/generate-aws-creds.sh
 
 echo "Running test suite..."
 
-docker run --rm --env-file /tmp/eks-workshop-shell-env \
+$CONTAINER_CLI run --rm --env-file ${TEMP:-/tmp}/eks-workshop-shell-env \
   -v $SCRIPT_DIR/../website/docs:/content \
   -v $SCRIPT_DIR/../environment/workspace:/workspace \
-  -e "AWS_ACCESS_KEY_ID" -e "AWS_SECRET_ACCESS_KEY" -e "AWS_SESSION_TOKEN" -e "AWS_DEFAULT_REGION" \
+  -e AWS_ACCESS_KEY_ID="$AWS_ACCESS_KEY_ID" -e AWS_SECRET_ACCESS_KEY="$AWS_SECRET_ACCESS_KEY" -e AWS_SESSION_TOKEN="$AWS_SESSION_TOKEN" -e AWS_DEFAULT_REGION="$AWS_DEFAULT_REGION" \
   $container_image -g "{$module,$module/**}" --hook-timeout 600 --timeout 1200 ${AWS_EKS_WORKSHOP_TEST_FLAGS}
