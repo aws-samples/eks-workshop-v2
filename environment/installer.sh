7718e0ceddd1105ea56fc7d8a7530172d43847a3
Carlos Santana <csantana23@gmail.com>
2023-06-07 09:45:46 -0400

    setup autocompletion and alias k
    Signed-off-by: Carlos Santana <csantana23@gmail.com>

--- environment/installer.sh
+++ environment/installer.sh
@@ -49,13 +49,7 @@ pip3 install awscurl
 download_and_verify "https://dl.k8s.io/release/v$kubectl_version/bin/linux/amd64/kubectl" "$kubectl_checksum" "kubectl"
 chmod +x ./kubectl
 mv ./kubectl /usr/local/bin
-kubectl completion bash > ~/.kube/completion.bash.inc
-printf "
-# Kubectl shell completion
-source '$HOME/.kube/completion.bash.inc'
-alias k=kubectl
-complete -F __start_kubectl k
-" >> $HOME/.bash_profile
+
 
 # helm
 download_and_verify "https://get.helm.sh/helm-v$helm_version-linux-amd64.tar.gz" "$helm_checksum" "helm.tar.gz"
