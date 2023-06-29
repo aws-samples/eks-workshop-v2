04d4064efa2f8548914537e0f200f9048d83e006
Rodrigo Bersa <rodrigo.bersa@gmail.com>
2023-06-28 20:26:13 -0400

    Changing AWS LBC module source repository to fix IAM Policy issue.

--- manifests/modules/exposing/ingress/.workshop/terraform/addon.tf
+++ manifests/modules/exposing/ingress/.workshop/terraform/addon.tf
@@ -1,4 +1,4 @@
 module "aws-load-balancer-controller" {
-  source        = "github.com/aws-ia/terraform-aws-eks-blueprints?ref=v4.25.0//modules/kubernetes-addons/aws-load-balancer-controller"
+  source        = "github.com/aws-ia/terraform-aws-eks-blueprints?ref=blueprints-workshops//modules/kubernetes-addons/aws-load-balancer-controller"
   addon_context = merge(local.addon_context, { default_repository = local.amazon_container_image_registry_uris[data.aws_region.current.name] })
 }
