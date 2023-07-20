88f361961e3e1dde4bc5c7e04e64b0f53acb61d9
Niall Thomson <thomson.niall@gmail.com>
2023-07-20 21:07:53 +0000

    Migrated to new eks blueprints addons module for lb controller

--- manifests/modules/autoscaling/compute/karpenter/.workshop/terraform/addon.tf
+++ manifests/modules/autoscaling/compute/karpenter/.workshop/terraform/addon.tf
@@ -12,9 +12,16 @@ module "karpenter" {
   }
 }
 
-module "aws-load-balancer-controller" {
-  source        = "github.com/aws-ia/terraform-aws-eks-blueprints?ref=v4.25.0//modules/kubernetes-addons/aws-load-balancer-controller"
-  addon_context = merge(local.addon_context, { default_repository = local.amazon_container_image_registry_uris[data.aws_region.current.name] })
+module "eks_blueprints_addons" {
+  source = "aws-ia/eks-blueprints-addons/aws"
+  version = "~> 1.0"
+
+  enable_aws_load_balancer_controller = true
+
+  cluster_name      = local.addon_context.eks_cluster_id
+  cluster_endpoint  = local.addon_context.aws_eks_cluster_endpoint
+  cluster_version   = local.eks_cluster_version
+  oidc_provider_arn = local.addon_context.eks_oidc_provider_arn
 }
 
 resource "aws_iam_instance_profile" "karpenter_node" {
