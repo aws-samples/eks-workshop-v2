bf4d32b16410f37bdb838eb41c61d7a2196545ad
Niall Thomson <thomson.niall@gmail.com>
2023-07-24 23:49:30 +0000

    Fix cleanup for ArgoCD to wait until apps deleted

--- manifests/modules/automation/gitops/argocd/.workshop/cleanup.sh
+++ manifests/modules/automation/gitops/argocd/.workshop/cleanup.sh
@@ -2,6 +2,6 @@
 
 echo "Deleting ArgoCD applications..."
 
-delete-all-if-crd-exists applications.argoproj.io
+delete-all-and-wait-if-crd-exists applications.argoproj.io
 
 rm -rf ~/environment/argocd
