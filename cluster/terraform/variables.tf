5b5ff1cba798b3d72006cd92f30da95ce581ad2b
Rodrigo Bersa <rodrigo.bersa@gmail.com>
2023-08-16 17:05:04 -0400

    Adding `cluster/terraform` deployment

---
+++ cluster/terraform/variables.tf
@@ -0,0 +1,34 @@
+variable "environment_name" {
+  type    = string
+  default = "eks-workshop"
+}
+
+variable "cluster_version" {
+  description = "EKS cluster version."
+  type        = string
+  default     = "1.25"
+}
+
+variable "ami_release_version" {
+  description = "Default EKS AMI release version for node groups"
+  type        = string
+  default     = " 1.25.6-20230304"
+}
+
+variable "vpc_cidr" {
+  description = "Defines the CIDR block used on Amazon VPC created for Amazon EKS."
+  type        = string
+  default     = "10.42.0.0/16"
+}
+
+variable "public_subnet_tags" {
+  description = "Tags to be added specifically on public facing subnets."
+  type        = map(string)
+  default     = {}
+}
+
+variable "private_subnet_tags" {
+  description = "Tags to be added specifically on private subnets."
+  type        = map(string)
+  default     = {}
+}
