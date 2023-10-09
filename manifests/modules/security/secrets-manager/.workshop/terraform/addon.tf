ac01951f36e508e8e1a49eb43bc5ac8a83cf1421
Rodrigo Bersa <rodrigo.bersa@gmail.com>
2023-10-09 15:05:45 -0400

    Fixing Addon, and validation steps

--- manifests/modules/security/secrets-manager/.workshop/terraform/addon.tf
+++ manifests/modules/security/secrets-manager/.workshop/terraform/addon.tf
@@ -1,8 +1,6 @@
 module "secrets-store-csi-driver" {
   source = "github.com/aws-ia/terraform-aws-eks-blueprints?ref=v4.32.1//modules/kubernetes-addons/secrets-store-csi-driver"
 
-  enable_secrets_store_csi_driver = true
-
   helm_config = {
     version = "1.3.4"
   }
@@ -11,9 +9,7 @@ module "secrets-store-csi-driver" {
 }
 
 module "secrets_store_csi_driver_provider_aws" {
-  source = "github.com/aws-ia/terraform-aws-eks-blueprints?ref=v4.32.1//modules/kubernetes-addons/secrets_store_csi_driver_provider_aws"
-
-  enable_secrets_store_csi_driver_provider_aws = true
+  source = "github.com/aws-ia/terraform-aws-eks-blueprints?ref=v4.32.1//modules/kubernetes-addons/csi-secrets-store-provider-aws"
 
   helm_config = {
     version = "0.3.4"
