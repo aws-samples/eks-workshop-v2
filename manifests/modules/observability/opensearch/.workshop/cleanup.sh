d6dd590322331629eea6241bf940ac0555249bae
Arvind Viswanathan <rvnd@amazon.com>
2023-10-19 16:57:02 -0700

    Fix: Update cleanup.sh

--- manifests/modules/observability/opensearch/.workshop/cleanup.sh
+++ manifests/modules/observability/opensearch/.workshop/cleanup.sh
@@ -2,4 +2,4 @@
 
 echo "Deleting OpenSearch exporter and test workloads..."
 helm uninstall events-to-opensearch -n opensearch-exporter > /dev/null 2>&1
-kubectl delete ns o11y-test > /dev/null 2>&1
+kubectl delete ns test > /dev/null 2>&1
