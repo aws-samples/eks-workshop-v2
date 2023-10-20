5eafe38e50eb8e3efd8f606529b8155acbf00904
Math Bruneau <brunemat@amazon.com>
2023-10-20 16:58:51 -0400

    In case the logs were never enable the command would fail so ignoring errors

--- manifests/modules/observability/logging/cluster/.workshop/cleanup.sh
+++ manifests/modules/observability/logging/cluster/.workshop/cleanup.sh
@@ -7,6 +7,6 @@ echo "Disabling EKS control plane logs..."
 aws eks update-cluster-config \
     --region $AWS_REGION \
     --name $EKS_CLUSTER_NAME \
-    --logging '{"clusterLogging":[{"types":["api","audit","authenticator","controllerManager","scheduler"],"enabled":false}]}' > /dev/null
+    --logging '{"clusterLogging":[{"types":["api","audit","authenticator","controllerManager","scheduler"],"enabled":false}]}' || true > /dev/null
 
 aws eks wait cluster-active --name $EKS_CLUSTER_NAME > /dev/null
