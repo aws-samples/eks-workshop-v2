902eac5f6916f149e6b1f4cf53ff3a3f665cab52
Niall Thomson <thomson.niall@gmail.com>
2023-10-25 17:02:41 +0000

    Ensure EBS addon using correct IAM role

--- manifests/modules/fundamentals/storage/ebs/.workshop/terraform/addon.tf
+++ manifests/modules/fundamentals/storage/ebs/.workshop/terraform/addon.tf
@@ -1,3 +1,21 @@
+module "ebs_csi_driver_irsa" {
+  source  = "terraform-aws-modules/iam/aws//modules/iam-role-for-service-accounts-eks"
+  version = "~> 5.20"
+
+  role_name_prefix = "${local.eks_cluster_id}-ebs-csi-driver-"
+
+  attach_ebs_csi_policy = true
+
+  oidc_providers = {
+    main = {
+      provider_arn               = local.eks_oidc_provider_arn
+      namespace_service_accounts = ["kube-system:ebs-csi-controller-sa"]
+    }
+  }
+
+  tags = local.tags
+}
+
 module "eks_blueprints_addons" {
   source = "aws-ia/eks-blueprints-addons/aws"
   version = "1.9.2"
@@ -9,7 +27,9 @@ module "eks_blueprints_addons" {
 
   eks_addons = {
     aws-ebs-csi-driver = {
-      most_recent = true
+      most_recent              = true
+      service_account_role_arn = module.ebs_csi_driver_irsa.iam_role_arn
+      preserve                 = false
     }
   }
 }
