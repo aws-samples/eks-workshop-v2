41d4360bfd0e17ff94af9b7306c25610ae1ee9bb
Arvind Viswanathan <rvnd@amazon.com>
2023-10-25 15:03:16 -0700

    Fix: cleanup up opensearch-exporter namespace

--- manifests/modules/observability/opensearch/.workshop/cleanup.sh
+++ manifests/modules/observability/opensearch/.workshop/cleanup.sh
@@ -2,5 +2,5 @@
 
 echo "Deleting OpenSearch exporter and test workloads..."
 helm uninstall events-to-opensearch -n opensearch-exporter > /dev/null 2>&1
-kubectl delete events-to-opensearch > /dev/null 2>&1
+kubectl delete ns opensearch-exporter > /dev/null 2>&1
 kubectl delete ns test > /dev/null 2>&1
