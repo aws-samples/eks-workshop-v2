37571c898ad8c4a903f313be11f4b48d50853969
7navyasa <navyasa@amazon.com>
2023-10-29 09:41:13 +1100

    upbound aws provider config addition

--- manifests/modules/automation/controlplanes/crossplane/.workshop/terraform/addon.tf
+++ manifests/modules/automation/controlplanes/crossplane/.workshop/terraform/addon.tf
@@ -11,22 +11,33 @@ module "crossplane" {
     terraform_data.cluster
   ]
 
-  source        = "github.com/aws-ia/terraform-aws-eks-blueprints?ref=v4.32.1//modules/kubernetes-addons/crossplane"
-  addon_context = local.addon_context
+  source  = "aws-ia/eks-blueprints-addon/aws"
+  version = "1.1.0"
 
-  helm_config = {
-    name             = "crossplane"
-    chart            = "crossplane"
-    repository       = "https://charts.crossplane.io/stable/"
-    version          = "1.12.1"
-    namespace        = "crossplane-system"
-    timeout          = 1200
-    create_namespace = true
-    values           = [templatefile("${path.module}/templates/crossplane.yaml",{})]
+  create = true
+
+  # https://github.com/crossplane/crossplane/tree/master/cluster/charts/crossplane
+  name             = "crossplane"
+  description      = "A Helm chart to deploy crossplane project"
+  namespace        = "crossplane-system"
+  create_namespace = true
+  chart            = "crossplane"
+  chart_version    = "1.13.2"
+  repository       = "https://charts.crossplane.io/stable/"
+  values           = templatefile("${path.module}/templates/crossplane.yaml", [])
+  
+
+  upbound_aws_provider = {
+    enable               = true
+    version              = "v0.40.0"
+    provider_config_name = "aws-provider-config" 
+    families = [
+      "dynamodb"
+    ]
   }
 
   aws_provider = {
-    enable                   = true
+    enable                   = false
     provider_aws_version     = "v0.40.0"
     additional_irsa_policies = ["arn:aws:iam::aws:policy/AdministratorAccess"]
   }
@@ -50,6 +61,71 @@ module "crossplane" {
     enable = false
   }
 }
+
+resource "kubectl_manifest" "upbound_aws_controller_config" {
+  count = local.upbound_aws_provider.enable == true ? 1 : 0
+  yaml_body = templatefile("${path.module}/providers/aws-upbound/controller-config.yaml", {
+    iam-role-arn          = module.upbound_irsa_aws[0].iam_role_arn
+    controller-config = local.upbound_aws_provider.controller_config
+  })
+
+  depends_on = [module.crossplane]
+}
+
+resource "kubectl_manifest" "upbound_aws_provider" {
+  for_each = local.upbound_aws_provider.enable ? toset(local.upbound_aws_provider.families) : toset([])
+  yaml_body = templatefile("${path.module}/providers/aws-upbound/provider.yaml", {
+    family            = each.key
+    version           = local.upbound_aws_provider.version
+    controller-config = local.upbound_aws_provider.controller_config
+  })
+  wait = true
+
+  depends_on = [kubectl_manifest.upbound_aws_controller_config]
+}
+
+# Wait for the Upbound AWS Provider CRDs to be fully created before initiating upbound_aws_provider_config
+resource "time_sleep" "upbound_wait_60_seconds" {
+  count           = local.upbound_aws_provider.enable == true ? 1 : 0
+  create_duration = "60s"
+
+  depends_on = [kubectl_manifest.upbound_aws_provider]
+}
+
+resource "kubectl_manifest" "upbound_aws_provider_config" {
+  count = local.upbound_aws_provider.enable == true ? 1 : 0
+  yaml_body = templatefile("${path.module}/providers/aws-upbound/provider-config.yaml", {
+    provider-config-name = local.upbound_aws_provider.provider_config_name
+  })
+
+  depends_on = [kubectl_manifest.upbound_aws_provider, time_sleep.upbound_wait_60_seconds]
+}
+
+
+resource "aws_iam_policy" "carts_dynamo" {
+  name        = "${local.addon_context.eks_cluster_id}-carts-dynamo"
+  path        = "/"
+  description = "DynamoDB policy for AWS Sample Carts Application"
+
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Sid": "AllAPIActionsOnCart",
+      "Effect": "Allow",
+      "Action": "dynamodb:*",
+      "Resource": [
+        "arn:aws:dynamodb:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:*"        
+      ]
+    }
+  ]
+}
+EOF
+  tags   = local.tags
+}
+
+
 
 module "eks_blueprints_addons" {
   source = "aws-ia/eks-blueprints-addons/aws"
