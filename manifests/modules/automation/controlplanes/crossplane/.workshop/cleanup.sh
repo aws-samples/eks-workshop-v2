53b4dbce6ae84fcb0955c8238833d7d6236d758b
7navyasa <navyasa@amazon.com>
2023-10-30 20:51:43 +1100

    crossplane cleanup script update

--- manifests/modules/automation/controlplanes/crossplane/.workshop/cleanup.sh
+++ manifests/modules/automation/controlplanes/crossplane/.workshop/cleanup.sh
@@ -2,12 +2,12 @@
 
 echo "Deleting resources created by Crossplane... (this will take several minutes)"
 
-delete-all-if-crd-exists relationaldatabases.awsblueprints.io
+delete-all-if-crd-exists dynamodbtables.awsblueprints.io
+
+delete-all-if-crd-exists xdynamodbtables.awsblueprints.io
 
 kubectl delete -k /eks-workshop/manifests/modules/automation/controlplanes/crossplane/managed/ --ignore-not-found=true > /dev/null
 
-kubectl wait --for=delete dbinstance.rds.aws.crossplane.io -l app.kubernetes.io/created-by=eks-workshop --timeout=600s > /dev/null
-kubectl wait --for=delete securitygroup.ec2.aws.crossplane.io -l app.kubernetes.io/created-by=eks-workshop --timeout=300s > /dev/null
-kubectl wait --for=delete dbsubnetgroup.database.aws.crossplane.io -l app.kubernetes.io/created-by=eks-workshop --timeout=300s > /dev/null
+kubectl delete -k /eks-workshop/manifests/modules/automation/controlplanes/crossplane/compositions/composition --ignore-not-found=true > /dev/null
 
 exit 0
