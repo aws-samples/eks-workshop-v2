1bee92c8138a73fb6c22c83c3e73958a64ca6bed
Niall Thomson <thomson.niall@gmail.com>
2023-10-30 16:40:29 +0000

    Release show use content labels, added more documentation

--- releaser/index.js
+++ releaser/index.js
@@ -1,22 +1,6 @@
 import { Octokit } from "@octokit/rest";
 
-let repository = process.env.GITHUB_REPOSITORY
-let sha = process.env.GITHUB_SHA
-let milestoneNumber = process.env.MILESTONE_NUMBER
-
-let auth = process.env.GITHUB_TOKEN
-
-let repositoryParts = repository.split('/');
-let owner = repositoryParts[0];
-let repo = repositoryParts[1];
-
-let tagName = `release-${Math.floor(Date.now() / 1000)}`
-
-const octokit = new Octokit({
-  auth
-});
-
-const categories = [{
+const CATEGORIES = [{
   title: "## 🚀 New labs",
   label: 'new'
 }, {
@@ -28,81 +12,127 @@ const categories = [{
 }, {
   title: "## 🧪 Features",
   label: 'feat'
-}]
+}];
 
-let entries = {};
+const CONTENT_LABEL_PREFIX = 'content/'
 
-let milestone = await octokit.rest.issues.getMilestone({
-  owner,
-  repo,
-  milestone_number: milestoneNumber,
-});
+function parseRepositoryString(repository) {
+  let repositoryParts = repository.split('/');
 
-for await (const response of octokit.paginate.iterator(
-  octokit.rest.pulls.list,
-  {
-    owner,
-    repo,
-    state: 'closed'
-  },
-)) {
-  response.data.forEach((e) => {
-    if(e.milestone) {
-      if(e.milestone.number == milestoneNumber) {
-        categories.forEach((c) => {
-          let targetPrefix = `${c.label}:`;
+  if(repositoryParts.length !== 2) {
+    throw new Error(`Repository ${repository} is not valid`);
+  }
 
-          if(e.title.indexOf(targetPrefix) == 0) {
-            if(!entries[c.title]) {
-              entries[c.title] = [];
-            }
-
-            entries[c.title].push({
-              number: e.number,
-              title: e.title.substring(targetPrefix.length + 1),
-              url: e.html_url,
-              author: {
-                login: e.user.login,
-                url: e.user.html_url,
-              }
-            });
-          }
-        })
-      }
-    }
-  })
+  return { owner: repositoryParts[0], repo: repositoryParts[1]};
 }
 
-let output = '';
+function generateMarkdown(entries) {
+  let output = '';
 
-categories.forEach((c) => {
-  let categoryOutput = `${c.title}
+  CATEGORIES.forEach((c) => {
+    let categoryOutput = `${c.title}
 
 `;
-  if(entries[c.title]) {
-    let categoryEntries = entries[c.title];
+    if(entries[c.title]) {
+      let categoryEntries = entries[c.title];
 
-    if(categoryEntries.length > 0) {
-      categoryEntries.forEach((e) => {
-        categoryOutput += `- ${e.title} by [@${e.author.login}](${e.author.url}) ([#${e.number}](${e.url}))\n`
-      });
+      if(categoryEntries.length > 0) {
+        categoryEntries.forEach((e) => {
+          categoryOutput += `- ${e.title} by [@${e.author.login}](${e.author.url}) ([#${e.number}](${e.url}))\n`
+        });
 
-      output += `${categoryOutput} \n`;
+        output += `${categoryOutput} \n`;
+      }
     }
+  });
+
+  return output;
+}
+
+async function main() {
+  let repository = process.env.GITHUB_REPOSITORY
+  let sha = process.env.GITHUB_SHA
+  let milestoneNumber = process.env.MILESTONE_NUMBER
+
+  let auth = process.env.GITHUB_TOKEN
+
+  let { owner, repo } = parseRepositoryString(repository);
+
+  let tagName = `release-${Math.floor(Date.now() / 1000)}`
+
+  const octokit = new Octokit({
+    auth
+  });
+
+  let entries = {};
+
+  let milestone = await octokit.rest.issues.getMilestone({
+    owner,
+    repo,
+    milestone_number: milestoneNumber,
+  });
+
+  for await (const response of octokit.paginate.iterator(
+    octokit.rest.pulls.list,
+    {
+      owner,
+      repo,
+      state: 'closed'
+    },
+  )) {
+    response.data.forEach((e) => {
+      if(e.milestone) {
+        if(e.milestone.number == milestoneNumber) {
+          const contentLabels = e.labels.filter((e) => e.name.indexOf(CONTENT_LABEL_PREFIX) == 0);
+          let contentArea = '';
+
+          if(contentLabels.length > 0) {
+            let contentAreaLabel = contentLabels[0].name.substring(CONTENT_LABEL_PREFIX.length);
+            contentArea = `[${contentAreaLabel.charAt(0).toUpperCase() + contentAreaLabel.slice(1)}] `
+          }
+
+          CATEGORIES.forEach((c) => {
+            let targetPrefix = `${c.label}:`;
+
+            if(e.title.indexOf(targetPrefix) == 0) {
+              if(!entries[c.title]) {
+                entries[c.title] = [];
+              }
+
+              entries[c.title].push({
+                number: e.number,
+                title: `${contentArea}${e.title.substring(targetPrefix.length + 1)}`,
+                url: e.html_url,
+                author: {
+                  login: e.user.login,
+                  url: e.user.html_url,
+                }
+              });
+            }
+          })
+        }
+      }
+    })
   }
-});
 
-await octokit.rest.git.createRef({
-  owner,
-  repo,
-  ref: `refs/tags/${tagName}`,
-  sha,
-});
+  let output = generateMarkdown(entries);
 
-await octokit.rest.repos.createRelease({
-  owner,
-  repo,
-  tag_name: tagName,
-  name: milestone.data.title,
-  body: output
-});
+  console.log(output);
+
+  /*await octokit.rest.git.createRef({
+    owner,
+    repo,
+    ref: `refs/tags/${tagName}`,
+    sha,
+  });
+
+  await octokit.rest.repos.createRelease({
+    owner,
+    repo,
+    tag_name: tagName,
+    name: milestone.data.title,
+    body: output
+  });*/
+}
+
+await main();
