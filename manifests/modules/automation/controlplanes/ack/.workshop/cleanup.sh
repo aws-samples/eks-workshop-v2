99050231c233cbaa5153371152b24da32ebc4348
Niall Thomson <thomson.niall@gmail.com>
2023-10-31 18:06:44 +0000

    Ensure ACK dynamo table and IAM role are named appropriately

--- manifests/modules/automation/controlplanes/ack/.workshop/cleanup.sh
+++ manifests/modules/automation/controlplanes/ack/.workshop/cleanup.sh
@@ -4,5 +4,3 @@ echo "Deleting resources created by ACK..."
 
 eksctl delete iamserviceaccount --name carts-ack --namespace carts --cluster $EKS_CLUSTER_NAME -v 0 > /dev/null
 kubectl delete table items -n carts --ignore-not-found=true > /dev/null
-kubectl delete namespace ack-dynamodb --ignore-not-found=true > /dev/null
-
