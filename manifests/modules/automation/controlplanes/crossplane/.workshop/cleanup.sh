8de2426cbf679ca50c26fed8670890c87f06cd79
7navyasa <navyasa@amazon.com>
2023-10-31 18:26:47 +1100

    updating crossplane cleanup script

--- manifests/modules/automation/controlplanes/crossplane/.workshop/cleanup.sh
+++ manifests/modules/automation/controlplanes/crossplane/.workshop/cleanup.sh
@@ -2,9 +2,7 @@
 
 echo "Deleting resources created by Crossplane... (this will take several minutes)"
 
-eksctl delete iamserviceaccount --name carts-crossplane  --namespace carts --cluster $EKS_CLUSTER_NAME
-
-kubectl delete -k /eks-workshop/manifests/modules/automation/controlplanes/crossplane/managed/ --ignore-not-found=true > /dev/null
+eksctl delete iamserviceaccount --name carts-ack --namespace carts --cluster $EKS_CLUSTER_NAME -v 0 > /dev/null
 
 kubectl delete table items -n carts --ignore-not-found=true > /dev/null
 
