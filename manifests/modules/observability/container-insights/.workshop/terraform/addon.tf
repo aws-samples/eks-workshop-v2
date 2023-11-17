647bc1278d568e06dbc6ff540111deed83d2de31
Niall Thomson <thomson.niall@gmail.com>
2023-11-17 20:41:00 +0000

    Removed unneeded ADOT IAM policy and environment variable in collector config

--- manifests/modules/observability/container-insights/.workshop/terraform/addon.tf
+++ manifests/modules/observability/container-insights/.workshop/terraform/addon.tf
@@ -15,7 +15,7 @@ module "iam_assumable_role_adot_ci" {
   create_role                   = true
   role_name                     = "${local.addon_context.eks_cluster_id}-adot-collector-ci"
   provider_url                  = local.addon_context.eks_oidc_issuer_url
-  role_policy_arns              = ["arn:${data.aws_partition.current.partition}:iam::aws:policy/CloudWatchAgentServerPolicy", "arn:${data.aws_partition.current.partition}:iam::aws:policy/AmazonPrometheusFullAccess"]
+  role_policy_arns              = ["arn:${data.aws_partition.current.partition}:iam::aws:policy/CloudWatchAgentServerPolicy"]
   oidc_fully_qualified_subjects = ["system:serviceaccount:other:adot-collector-ci"]
 
   tags = local.tags
