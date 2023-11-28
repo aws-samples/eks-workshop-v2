b69263af746bd77153575d73bdf1c1697813297a
Niall Thomson <thomson.niall@gmail.com>
2023-11-28 13:11:19 -0700

    Update docusaurus.config.js

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -140,6 +140,12 @@ const config = {
             label: 'Automation',
           },
           {
+            type: 'doc',
+            docId: 'aiml/index',
+            position: 'left',
+            label: 'AI/ML',
+          },
+          {
             href: 'https://github.com/aws-samples/eks-workshop-v2',
             position: 'right',
             className: 'header-github-link',
