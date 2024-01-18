dfa139afc004fae4931545cb1b7a63ffae9f2011
Rodrigo Bersa <rodrigo.bersa@gmail.com>
2024-01-18 17:50:12 -0500

    Adding cleanup script

---
+++ manifests/modules/security/kyverno/.workshop/cleanup.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+set -e
+
+delete-all-if-crd-exists admissionreports.kyverno.io
+delete-all-if-crd-exists backgroundscanreports.kyverno.io
+delete-all-if-crd-exists cleanuppolicies.kyverno.io
+delete-all-if-crd-exists clusteradmissionreports.kyverno.io
+delete-all-if-crd-exists clusterbackgroundscanreports.kyverno.io
+delete-all-if-crd-exists clustercleanuppolicies.kyverno.io
+delete-all-if-crd-exists clusterpolicies.kyverno.io
+delete-all-if-crd-exists policies.kyverno.io
+delete-all-if-crd-exists policyexceptions.kyverno.io
+delete-all-if-crd-exists updaterequests.kyverno.io
+
+kubectl -n default delete pods --all
