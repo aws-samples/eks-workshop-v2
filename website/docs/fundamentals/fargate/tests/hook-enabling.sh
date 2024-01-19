94a9be7df1960df387e08ca0e2f224bcd14359e7
Niall Thomson <thomson.niall@gmail.com>
2024-01-19 03:30:11 +0000

    Added debug information to fargate test hook

--- website/docs/fundamentals/fargate/tests/hook-enabling.sh
+++ website/docs/fundamentals/fargate/tests/hook-enabling.sh
@@ -7,6 +7,7 @@ after() {
 
   if [ -z "$check" ]; then
     echo "checkout pod not scheduled on fargate"
+    kubectl get po -n checkout -l app.kubernetes.io/instance=checkout,app.kubernetes.io/component=service -o json | jq
     exit 1
   fi
 }
