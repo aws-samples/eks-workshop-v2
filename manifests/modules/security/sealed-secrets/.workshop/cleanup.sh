b48d84837e544e3b168f096e331219f2d4b9e395
Rodrigo Bersa <rodrigo.bersa@gmail.com>
2024-01-24 16:48:50 -0500

    Baseline main

--- manifests/modules/security/sealed-secrets/.workshop/cleanup.sh
+++ manifests/modules/security/sealed-secrets/.workshop/cleanup.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
-kubectl delete -f https://github.com/bitnami-labs/sealed-secrets/releases/download/v0.18.0/controller.yaml --ignore-not-found > /dev/null
+set -e
 
-kubectl delete -n catalog secret catalog-sealed-db --ignore-not-found > /dev/null
+kubectl delete -f https://github.com/bitnami-labs/sealed-secrets/releases/download/v0.18.0/controller.yaml --ignore-not-found
+
+kubectl delete -n catalog secret catalog-sealed-db --ignore-not-found
