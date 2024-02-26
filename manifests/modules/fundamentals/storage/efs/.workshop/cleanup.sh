ab7faea239ec963bba29dd2f9eb69cf1803d4081
erichei <erichei@amazon.com>
2024-02-26 13:07:53 -0600

    updated cleanup.sh for EFS module

--- manifests/modules/fundamentals/storage/efs/.workshop/cleanup.sh
+++ manifests/modules/fundamentals/storage/efs/.workshop/cleanup.sh
@@ -5,3 +5,13 @@ set -e
 logmessage "Deleting EFS storage class..."
 
 kubectl delete storageclass efs-sc --ignore-not-found
+
+check=$(aws eks list-addons --cluster-name $EKS_CLUSTER_NAME --query "addons[? @ == 'aws-efs-csi-driver']" --output text)
+
+if [ ! -z "$check" ]; then
+  logmessage "Deleting EFS CSI driver addon..."
+
+  aws eks delete-addon --cluster-name $EKS_CLUSTER_NAME --addon-name aws-efs-csi-driver
+
+  aws eks wait addon-deleted --cluster-name $EKS_CLUSTER_NAME --addon-name aws-efs-csi-driver
+fi
