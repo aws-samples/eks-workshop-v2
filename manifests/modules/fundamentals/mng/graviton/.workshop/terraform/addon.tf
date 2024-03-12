25fdce2446e8de896897850a4c06aa74b92b05fb
Niall Thomson <thomson.niall@gmail.com>
2024-03-11 20:00:01 -0600

    Use AWS CLI for Graviton nodes instead of eksctl (#848)

--- manifests/modules/fundamentals/mng/graviton/.workshop/terraform/addon.tf
+++ manifests/modules/fundamentals/mng/graviton/.workshop/terraform/addon.tf
@@ -10,8 +10,36 @@ data "aws_subnets" "private" {
   }
 }
 
+resource "aws_iam_role" "graviton_node" {
+  name = "${local.addon_context.eks_cluster_id}-graviton-node"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Sid    = ""
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      },
+    ]
+  })
+
+  managed_policy_arns = [
+    "arn:${local.addon_context.aws_partition_id}:iam::aws:policy/AmazonEKS_CNI_Policy",
+    "arn:${local.addon_context.aws_partition_id}:iam::aws:policy/AmazonEKSWorkerNodePolicy",
+    "arn:${local.addon_context.aws_partition_id}:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly",
+    "arn:${local.addon_context.aws_partition_id}:iam::aws:policy/AmazonSSMManagedInstanceCore"
+  ]
+
+  tags = local.tags
+}
+
 output "environment" {
   value = <<EOF
+export GRAVITON_NODE_ROLE="${aws_iam_role.graviton_node.arn}"
 %{for index, id in data.aws_subnets.private.ids}
 export PRIMARY_SUBNET_${index + 1}=${id}
 %{endfor}
