79fcac91959eec64dd8817958b9d4a704b8cc95d
Niall Thomson <thomson.niall@gmail.com>
2024-03-21 21:31:23 +0000

    Fix EKS pod identity partition data source

--- manifests/modules/security/eks-pod-identity/.workshop/terraform/main.tf
+++ manifests/modules/security/eks-pod-identity/.workshop/terraform/main.tf
@@ -1,5 +1,6 @@
 data "aws_caller_identity" "current" {}
 data "aws_region" "current" {}
+data "aws_partition" "current" {}
 
 module "eks_blueprints_addons" {
   source  = "aws-ia/eks-blueprints-addons/aws"
