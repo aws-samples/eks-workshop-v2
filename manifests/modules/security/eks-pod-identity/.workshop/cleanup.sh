3b4340213ceafa4d5aac60d266f7618c9cec65ca
Niall Thomson <thomson.niall@gmail.com>
2024-04-05 16:19:32 +0000

    Fix pod identity cleanup for incomplete lab

--- manifests/modules/security/eks-pod-identity/.workshop/cleanup.sh
+++ manifests/modules/security/eks-pod-identity/.workshop/cleanup.sh
@@ -4,7 +4,7 @@ set -e
 
 POD_ASSOCIATION_ID=$(aws eks list-pod-identity-associations --region $AWS_REGION --cluster-name $EKS_CLUSTER_NAME --service-account carts --namespace carts --output text --query 'associations[0].associationId')
 
-if [ ! -z "$POD_ASSOCIATION_ID" ]; then
+if [ "$POD_ASSOCIATION_ID" != "None" ]; then
   logmessage "Deleting EKS Pod Identity Association..."
   
   aws eks delete-pod-identity-association --region $AWS_REGION --association-id $POD_ASSOCIATION_ID --cluster-name $EKS_CLUSTER_NAME
