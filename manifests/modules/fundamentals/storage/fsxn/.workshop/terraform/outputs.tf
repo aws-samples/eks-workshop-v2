c241b79795a30cea395558ed43cef355e28a4d4f
mickeysh <mickey.shaul@gmail.com>
2024-06-03 20:22:56 +0300

    update FSxN csi driver version and install method

--- manifests/modules/fundamentals/storage/fsxn/.workshop/terraform/outputs.tf
+++ manifests/modules/fundamentals/storage/fsxn/.workshop/terraform/outputs.tf
@@ -4,5 +4,7 @@ output "environment_variables" {
     FSXN_ID             = aws_fsx_ontap_file_system.fsxnassets.id
     FSXN_ADMIN_PASSWORD = random_string.fsx_password.result
     FSXN_IP             = tolist(aws_fsx_ontap_file_system.fsxnassets.endpoints[0].management[0].ip_addresses)[0]
+    FSXN_CSI_ADDON_ROLE = module.iam_iam-role-for-service-accounts-eks.iam_role_arn
   }
 }
+
