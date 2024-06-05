68ff4c6524851989c91937ea836ae56427f0d63e
Niall Thomson <thomson.niall@gmail.com>
2024-06-05 12:48:47 +0000

    Change how helm registryUrls are handled

--- manifests/modules/autoscaling/compute/cluster-autoscaler/.workshop/terraform/vars.tf
+++ manifests/modules/autoscaling/compute/cluster-autoscaler/.workshop/terraform/vars.tf
@@ -37,6 +37,6 @@ variable "resources_precreated" {
 variable "cluster_autoscaler_chart_version" {
   description = "The version of cluster-autoscaler to use"
   type        = string
-  # renovate: datasource=helm depName=cluster-autoscaler registryUrl=https://kubernetes.github.io/autoscaler
+  # renovate: datasource=helm depName=cluster-autoscaler
   default = "9.37.0"
 }
