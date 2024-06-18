1bf3c60f7413ee7bfde7d59b7f446519068bc7d3
Niall Thomson <thomson.niall@gmail.com>
2024-06-18 15:48:17 -0600

    Various local dev improvements

--- hack/lib/generate-aws-creds.sh
+++ hack/lib/generate-aws-creds.sh
@@ -1,6 +1,21 @@
-echo "Generating temporary AWS credentials..."
+aws_credential_args=""
 
-ACCESS_VARS=$(aws sts assume-role --role-arn $ASSUME_ROLE --role-session-name ${EKS_CLUSTER_NAME}-shell --output json | jq -r '.Credentials | "export AWS_ACCESS_KEY_ID=\(.AccessKeyId) AWS_SECRET_ACCESS_KEY=\(.SecretAccessKey) AWS_SESSION_TOKEN=\(.SessionToken)"')
+ASSUME_ROLE=${ASSUME_ROLE:-""}
+AWS_ACCESS_KEY_ID=${AWS_ACCESS_KEY_ID:-""}
 
-# TODO: This should probably not use eval
-eval "$ACCESS_VARS"
+if [ ! -z "$AWS_ACCESS_KEY_ID" ]; then
+  echo "Using environment AWS_ACCESS_KEY_ID/AWS_SECRET_ACCESS_KEY"
+
+  aws_credential_args="-e AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID -e AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY -e AWS_SESSION_TOKEN=$AWS_SESSION_TOKEN"
+elif [ ! -z "$ASSUME_ROLE" ]; then
+  echo "Generating temporary AWS credentials..."
+
+  ACCESS_VARS=$(aws sts assume-role --role-arn $ASSUME_ROLE --role-session-name ${EKS_CLUSTER_NAME}-shell --output json | jq -r '.Credentials | "export AWS_ACCESS_KEY_ID=\(.AccessKeyId) AWS_SECRET_ACCESS_KEY=\(.SecretAccessKey) AWS_SESSION_TOKEN=\(.SessionToken)"')
+
+  # TODO: This should probably not use eval
+  eval "$ACCESS_VARS"
+
+  aws_credential_args="-e AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID -e AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY -e AWS_SESSION_TOKEN=$AWS_SESSION_TOKEN"
+else
+  echo "Inheriting credentials from instance profile"
+fi
--- lab/scripts/installer.sh
+++ lab/scripts/installer.sh
@@ -12,7 +12,6 @@ helm_version='3.15.1'
 eksctl_version='0.180.0'
 
 kubeseal_version='0.18.4'
-kubeseal_checksum='2e765b87889bfcf06a6249cde8e28507e3b7be29851e4fac651853f7638f12f3'
 
 # renovate: depName=mikefarah/yq
 yq_version='4.44.1'
@@ -49,6 +48,23 @@ download_and_verify () {
   rm "$out_file.sha256"
 }
 
+arch=$(uname -m)
+arch_name=""
+
+# Convert to amd64 or arm64
+case "$arch" in
+  x86_64)
+    arch_name="amd64"
+    ;;
+  aarch64)
+    arch_name="arm64"
+    ;;
+  *)
+    echo "Unsupported architecture: $arch"
+    exit 1
+    ;;
+esac
+
 yum install --quiet -y findutils jq tar gzip zsh git diffutils wget \
   tree unzip openssl gettext bash-completion python3 pip3 python3-pip \
   amazon-linux-extras nc yum-utils
@@ -56,65 +72,65 @@ yum install --quiet -y findutils jq tar gzip zsh git diffutils wget \
 pip3 install -q awscurl==0.28 urllib3==1.26.6
 
 # kubectl
-download "https://dl.k8s.io/release/v$kubectl_version/bin/linux/amd64/kubectl" "kubectl"
+download "https://dl.k8s.io/release/v$kubectl_version/bin/linux/${arch_name}/kubectl" "kubectl"
 chmod +x ./kubectl
 mv ./kubectl /usr/local/bin
 
 # helm
-download "https://get.helm.sh/helm-v$helm_version-linux-amd64.tar.gz" "helm.tar.gz"
+download "https://get.helm.sh/helm-v$helm_version-linux-${arch_name}.tar.gz" "helm.tar.gz"
 tar zxf helm.tar.gz
-chmod +x linux-amd64/helm
-mv ./linux-amd64/helm /usr/local/bin
-rm -rf linux-amd64/ helm.tar.gz
+chmod +x linux-${arch_name}/helm
+mv ./linux-${arch_name}/helm /usr/local/bin
+rm -rf linux-${arch_name}/ helm.tar.gz
 
 # eksctl
-download "https://github.com/weaveworks/eksctl/releases/download/v$eksctl_version/eksctl_Linux_amd64.tar.gz" "eksctl_Linux_amd64.tar.gz"
-tar zxf eksctl_Linux_amd64.tar.gz
+download "https://github.com/weaveworks/eksctl/releases/download/v$eksctl_version/eksctl_Linux_${arch_name}.tar.gz" "eksctl.tar.gz"
+tar zxf eksctl.tar.gz
 chmod +x eksctl
 mv ./eksctl /usr/local/bin
-rm -rf eksctl_Linux_amd64.tar.gz
+rm -rf eksctl.tar.gz
 
 # aws cli v2
-curl --location --show-error --silent "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+curl --location --show-error --silent "https://awscli.amazonaws.com/awscli-exe-linux-${arch}.zip" -o "awscliv2.zip"
 unzip -o -q awscliv2.zip -d /tmp
 /tmp/aws/install --update
 rm -rf /tmp/aws awscliv2.zip
 
 # kubeseal
-download_and_verify "https://github.com/bitnami-labs/sealed-secrets/releases/download/v${kubeseal_version}/kubeseal-${kubeseal_version}-linux-amd64.tar.gz" "$kubeseal_checksum" "kubeseal.tar.gz"
+download "https://github.com/bitnami-labs/sealed-secrets/releases/download/v${kubeseal_version}/kubeseal-${kubeseal_version}-linux-${arch_name}.tar.gz" "kubeseal.tar.gz"
 tar xfz kubeseal.tar.gz
 chmod +x kubeseal
 mv ./kubeseal /usr/local/bin
 rm -rf kubeseal.tar.gz
 
 # yq
-download "https://github.com/mikefarah/yq/releases/download/v${yq_version}/yq_linux_amd64" "yq"
+download "https://github.com/mikefarah/yq/releases/download/v${yq_version}/yq_linux_${arch_name}" "yq"
 chmod +x ./yq
 mv ./yq /usr/local/bin
 
 # flux
-download "https://github.com/fluxcd/flux2/releases/download/v${flux_version}/flux_${flux_version}_linux_amd64.tar.gz" "flux.tar.gz"
+download "https://github.com/fluxcd/flux2/releases/download/v${flux_version}/flux_${flux_version}_linux_${arch_name}.tar.gz" "flux.tar.gz"
 tar zxf flux.tar.gz
 chmod +x flux
 mv ./flux /usr/local/bin
 rm -rf flux.tar.gz
 
 # terraform
-download "https://releases.hashicorp.com/terraform/${terraform_version}/terraform_${terraform_version}_linux_amd64.zip" "terraform.zip"
+download "https://releases.hashicorp.com/terraform/${terraform_version}/terraform_${terraform_version}_linux_${arch_name}.zip" "terraform.zip"
 unzip -o -q terraform.zip -d /tmp
 chmod +x /tmp/terraform
 mv /tmp/terraform /usr/local/bin
 rm -f terraform.zip
 
 # argocd
-download "https://github.com/argoproj/argo-cd/releases/download/v${argocd_version}/argocd-linux-amd64" "argocd-linux-amd64"
-chmod +x ./argocd-linux-amd64
-mv ./argocd-linux-amd64 /usr/local/bin/argocd
+download "https://github.com/argoproj/argo-cd/releases/download/v${argocd_version}/argocd-linux-${arch_name}" "argocd"
+chmod +x ./argocd
+mv ./argocd /usr/local/bin/argocd
 
 # ec2 instance selector
-download "https://github.com/aws/amazon-ec2-instance-selector/releases/download/v${ec2_instance_selector_version}/ec2-instance-selector-linux-amd64" "ec2-instance-selector-linux-amd64"
-chmod +x ./ec2-instance-selector-linux-amd64
-mv ./ec2-instance-selector-linux-amd64 /usr/local/bin/ec2-instance-selector
+download "https://github.com/aws/amazon-ec2-instance-selector/releases/download/v${ec2_instance_selector_version}/ec2-instance-selector-linux-${arch_name}" "ec2-instance-selector"
+chmod +x ./ec2-instance-selector
+mv ./ec2-instance-selector /usr/local/bin/ec2-instance-selector
 
 REPOSITORY_OWNER=${REPOSITORY_OWNER:-"aws-samples"}
 REPOSITORY_NAME=${REPOSITORY_NAME:-"eks-workshop-v2"}
