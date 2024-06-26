686e30c8c1763e6b4c2b8503beab87de3bd134ae
Mohanty <natmhnty@amazon.com>
2024-06-26 10:50:11 -0500

    added new configuration files

---
+++ manifests/modules/aiml/chatbot/.workshop/cleanup.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+set -e
+
+logmessage "Deleting AIML resources..."
+
+kubectl delete namespace aiml --ignore-not-found
+
+logmessage "Deleting Karpenter NodePool and EC2NodeClass..."
+
+delete-all-if-crd-exists nodepools.karpenter.sh
+delete-all-if-crd-exists ec2nodeclasses.karpenter.k8s.aws
+
+logmessage "Waiting for Karpenter nodes to be removed..."
+
+EXIT_CODE=0
+
+timeout --foreground -s TERM 30 bash -c \
+    'while [[ $(kubectl get nodes --selector=type=karpenter -o json | jq -r ".items | length") -gt 0 ]];\
+    do sleep 5;\
+    done' || EXIT_CODE=$?
+
+if [ $EXIT_CODE -ne 0 ]; then
+  logmessage "Warning: Karpenter nodes did not clean up"
+fi
---
+++ manifests/modules/aiml/chatbot/inference/inference.py
@@ -0,0 +1,55 @@
+import os
+import time
+import torch
+import torch_neuron
+import json
+import numpy as np
+
+from urllib import request
+
+from torchvision import models, transforms, datasets
+
+## Create an image directory containing a small kitten
+os.makedirs("./torch_neuron_test/images", exist_ok=True)
+request.urlretrieve("https://raw.githubusercontent.com/awslabs/mxnet-model-server/master/docs/images/kitten_small.jpg",
+                    "./torch_neuron_test/images/kitten_small.jpg")
+
+
+## Fetch labels to output the top classifications
+request.urlretrieve("https://s3.amazonaws.com/deep-learning-models/image-models/imagenet_class_index.json","imagenet_class_index.json")
+idx2label = []
+
+with open("imagenet_class_index.json", "r") as read_file:
+    class_idx = json.load(read_file)
+    idx2label = [class_idx[str(k)][1] for k in range(len(class_idx))]
+
+## Import a sample image and normalize it into a tensor
+normalize = transforms.Normalize(
+    mean=[0.485, 0.456, 0.406],
+    std=[0.229, 0.224, 0.225])
+
+eval_dataset = datasets.ImageFolder(
+    os.path.dirname("./torch_neuron_test/"),
+    transforms.Compose([
+    transforms.Resize([224, 224]),
+    transforms.ToTensor(),
+    normalize,
+    ])
+)
+
+image, _ = eval_dataset[0]
+image = torch.tensor(image.numpy()[np.newaxis, ...])
+
+## Load model
+model_neuron = torch.jit.load( 'resnet50_neuron.pt' )
+
+## Predict
+results = model_neuron( image )
+
+# Get the top 5 results
+top5_idx = results[0].sort()[1][-5:]
+
+# Lookup and print the top 5 labels
+top5_labels = [idx2label[idx] for idx in top5_idx]
+
+print("Top 5 labels:\n {}".format(top5_labels) )
