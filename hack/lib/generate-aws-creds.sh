66d078182946ef217005e96dc2ddb6d31ea9b847
Niall Thomson <thomson.niall@gmail.com>
2024-08-07 20:13:15 -0600

    Add back ability to assume role for hack scripts

--- hack/lib/generate-aws-creds.sh
+++ hack/lib/generate-aws-creds.sh
@@ -2,7 +2,16 @@ echo "Generating temporary AWS credentials..."
 
 session_suffix=$(openssl rand -hex 4)
 
-ACCESS_VARS=$(aws sts assume-role --role-arn ${IDE_ROLE_ARN} --role-session-name ${EKS_CLUSTER_NAME}-shell-${session_suffix} --output json | jq -r '.Credentials | "export AWS_ACCESS_KEY_ID=\(.AccessKeyId) AWS_SECRET_ACCESS_KEY=\(.SecretAccessKey) AWS_SESSION_TOKEN=\(.SessionToken)"')
+target_role=${IDE_ROLE_ARN}
+
+ASSUME_ROLE=${ASSUME_ROLE:-""}
+
+if [ ! -z "$ASSUME_ROLE" ]; then
+  echo "Assuming role $ASSUME_ROLE"
+  target_role=$ASSUME_ROLE
+fi
+
+ACCESS_VARS=$(aws sts assume-role --role-arn ${target_role} --role-session-name ${EKS_CLUSTER_NAME}-shell-${session_suffix} --output json | jq -r '.Credentials | "export AWS_ACCESS_KEY_ID=\(.AccessKeyId) AWS_SECRET_ACCESS_KEY=\(.SecretAccessKey) AWS_SESSION_TOKEN=\(.SessionToken)"')
 
 # TODO: This should probably not use eval
 eval "$ACCESS_VARS"
