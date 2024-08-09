74f21bedb71320f8020b5edc3b28d083dc7fc42a
Niall Thomson <thomson.niall@gmail.com>
2024-08-08 21:10:45 -0600

    Simplify destroy

--- hack/destroy-infrastructure.sh
+++ hack/destroy-infrastructure.sh
@@ -16,7 +16,7 @@ if [ $cluster_exists -eq 0 ]; then
   echo "Deleting cluster ${EKS_CLUSTER_NAME}"
   bash $SCRIPT_DIR/shell.sh "${environment}" 'delete-environment || true'
 
-  bash $SCRIPT_DIR/exec.sh "${environment}" 'cat /cluster/eksctl/cluster.yaml | envsubst | eksctl delete cluster --wait --force --disable-nodegroup-eviction --timeout 45m -f -'
+  bash $SCRIPT_DIR/exec.sh "${environment}" 'eksctl delete cluster --name ${EKS_CLUSTER_NAME} --region ${AWS_REGION} --wait --force --disable-nodegroup-eviction --timeout 45m'
 else
   echo "Cluster ${EKS_CLUSTER_NAME} does not exist"
 fi
