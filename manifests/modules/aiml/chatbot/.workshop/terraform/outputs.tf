14e668a233abf8fb25bdd4701d3f23be65f8af22
natmhnty <natmhnty@amazon.com>
2024-08-14 13:16:23 -0400

    removed LBC environment variables

--- manifests/modules/aiml/chatbot/.workshop/terraform/outputs.tf
+++ manifests/modules/aiml/chatbot/.workshop/terraform/outputs.tf
@@ -4,7 +4,5 @@ output "environment_variables" {
     AIML_SUBNETS        = "${data.aws_subnets.private.ids[0]},${data.aws_subnets.private.ids[1]},${data.aws_subnets.private.ids[2]}"
     KARPENTER_NODE_ROLE = module.eks_blueprints_addons.karpenter.node_iam_role_name
     KARPENTER_ARN       = module.eks_blueprints_addons.karpenter.node_iam_role_arn
-    LBC_CHART_VERSION   = var.load_balancer_controller_chart_version
-    LBC_ROLE_ARN        = module.eks_blueprints_addons.aws_load_balancer_controller.iam_role_arn
   }
 }
