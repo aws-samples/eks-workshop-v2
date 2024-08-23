9800fe41649c328395a0624a0595799e77b7f8f7
Rishiraj Chandra <rishircn@amazon.com>
2024-08-23 15:44:04 -0400

    Work in progress to create S3 workshop, pre-commit hooks skipped

---
+++ manifests/modules/fundamentals/storage/s3/.workshop/cleanup.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+
+# Delete addon
+eksctl delete addon --cluster $EKS_CLUSTER_NAME --name aws-mountpoint-s3-csi-driver --preserve
+
+set -e
+
+check=$(aws eks list-addons --cluster-name $EKS_CLUSTER_NAME --query "addons[? @ == 'aws-efs-csi-driver']" --output text)
+
+kubectl scale -n assets --replicas=0 deployment/assets
+
+logmessage "Deleting EFS storage class..."
+
+kubectl delete storageclass efs-sc --ignore-not-found
+
+if [ ! -z "$check" ]; then
+  logmessage "Deleting EFS CSI driver addon..."
+
+  aws eks delete-addon --cluster-name $EKS_CLUSTER_NAME --addon-name aws-efs-csi-driver
+
+  aws eks wait addon-deleted --cluster-name $EKS_CLUSTER_NAME --addon-name aws-efs-csi-driver
+fi
---
+++ manifests/modules/fundamentals/storage/s3/.workshop/terraform/main.tf
@@ -0,0 +1,39 @@
+# Get AWS account ID
+data "aws_caller_identity" "current" {}
+
+# Create a unique S3 bucket
+resource "aws_s3_bucket" "mountpoint-s3" {
+   bucket = "mountpoint-s3-${data.aws_caller_identity.current.account_id}"
+}
+
+# S3 CSI Driver IAM Role
+module "mountpoint_s3_csi_driver_irsa" {
+  source = "terraform-aws-modules/iam/aws//modules/iam-role-for-service-accounts-eks"
+  version = "5.39.1"
+
+  role_name_prefix = "${var.addon_context.eks_cluster_id}-s3-csi-"
+  
+  # IAM policy to attach to driver
+  attach_mountpoint_s3_csi_policy = true
+
+  mountpoint_s3_csi_bucket_arns   = [aws_s3_bucket.mountpoint-s3.arn]
+  mountpoint_s3_csi_path_arns     = ["${aws_s3_bucket.mountpoint-s3.arn}/*"]
+
+  oidc_providers = {
+    main = {
+      provider_arn               = var.addon_context.eks_oidc_provider_arn
+      namespace_service_accounts = ["kube-system:s3-csi-driver-sa"]
+    }
+  }
+
+  tags = var.tags
+}
+
+# Mountpoint S3 CSI Add-On
+# This might be just a command to run so may not be needed
+# resource "aws_eks_addon" "mountpoint_s3_csi_addon" {
+#   cluster_name  = var.addon_context.eks_cluster_id
+#   addon_name    = "aws-mountpoint-s3-csi-driver"
+#   addon_version = "v1.7.0-eksbuild.1"
+# }
+
