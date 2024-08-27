a185f45deb51060e4b4a9cfe4a9d44742b0d331c
Pongsan <pongsan@amazon.com>
2024-08-27 13:19:54 +0800

    Remove old OSS and Kubecost modules.

--- manifests/modules/observability/kubecost/.workshop/cleanup.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-set -e
-
-uninstall-helm-chart kubecost kubecost
--- manifests/modules/observability/kubecost/.workshop/terraform/outputs.tf
+++
@@ -1,6 +0,0 @@
-output "environment_variables" {
-  description = "Environment variables to be added to the IDE shell"
-  value = {
-    KUBECOST_CHART_VERSION = var.kubecost_chart_version
-  }
-}
