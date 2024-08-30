9dc3ef96c983542c13bee93aabd7dc74a7b23b99
Niall Thomson <thomson.niall@gmail.com>
2024-08-30 15:24:52 -0600

    Remove 1.30 upgrade header

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -91,13 +91,6 @@ const config = {
   themeConfig:
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
-      announcementBar: {
-        id: "upgrade-1.30",
-        content:
-          '🚩 EKS Workshop upgraded to EKS 1.30 on 26th July. If you have an existing lab environment please see the <a target="_blank" rel="noopener noreferrer" href="/docs/misc/major-upgrade">major upgrade instructions</a>. 🚩',
-        backgroundColor: "#0972d3",
-        textColor: "#fff",
-      },
       colorMode: {
         defaultMode: "light",
         disableSwitch: false,
