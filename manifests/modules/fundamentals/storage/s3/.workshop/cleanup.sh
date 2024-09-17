4760230f2271e4a8c3424954e3e686e22cff271a
Rishiraj Chandra <rishircn@amazon.com>
2024-09-17 10:13:16 -0400

    Experimenting with different deployments to scale replica

--- manifests/modules/fundamentals/storage/s3/.workshop/cleanup.sh
+++ manifests/modules/fundamentals/storage/s3/.workshop/cleanup.sh
@@ -3,23 +3,20 @@
 
 # Ensure cleanup is all good before deploying!
 
-# Delete addon
-eksctl delete addon --cluster $EKS_CLUSTER_NAME --name aws-mountpoint-s3-csi-driver --preserve
-
 set -e
 
-check=$(aws eks list-addons --cluster-name $EKS_CLUSTER_NAME --query "addons[? @ == 'aws-efs-csi-driver']" --output text)
-
-kubectl scale -n assets --replicas=0 deployment/assets
-
-logmessage "Deleting EFS storage class..."
+check=$(aws eks list-addons --cluster-name $EKS_CLUSTER_NAME --query "addons[? @ == 'aws-mountpoint-s3-csi-driver']" --output text)
 
 kubectl delete storageclass efs-sc --ignore-not-found
 
+# Scale down assets
+kubectl scale -n assets --replicas=0 deployment/assets
+
+# Delete the S3 CSI driver addon
 if [ ! -z "$check" ]; then
-  logmessage "Deleting EFS CSI driver addon..."
+  logmessage "Deleting S3 CSI driver addon..."
 
-  aws eks delete-addon --cluster-name $EKS_CLUSTER_NAME --addon-name aws-efs-csi-driver
+  aws eks delete-addon --cluster-name $EKS_CLUSTER_NAME --addon-name aws-mountpoint-s3-csi-driver
 
-  aws eks wait addon-deleted --cluster-name $EKS_CLUSTER_NAME --addon-name aws-efs-csi-driver
+  aws eks wait addon-deleted --cluster-name $EKS_CLUSTER_NAME --addon-name aws-mountpoint-s3-csi-driver
 fi
