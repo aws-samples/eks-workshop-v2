59fffc4a287d5a5121cde01cae06d51dc8d89ca7
Sai Vennam <svennam92@gmail.com>
2024-09-26 16:46:25 -0500

    Fix iam issues in AI/ML chatbot PR and update to Karpenter v1 (#1110)

--- manifests/modules/aiml/chatbot/.workshop/terraform/outputs.tf
+++ manifests/modules/aiml/chatbot/.workshop/terraform/outputs.tf
@@ -2,7 +2,7 @@ output "environment_variables" {
   description = "Environment variables to be added to the IDE shell"
   value = {
     AIML_SUBNETS        = "${data.aws_subnets.private.ids[0]},${data.aws_subnets.private.ids[1]},${data.aws_subnets.private.ids[2]}"
-    KARPENTER_NODE_ROLE = module.eks_blueprints_addons.karpenter.node_iam_role_name
-    KARPENTER_ARN       = module.eks_blueprints_addons.karpenter.node_iam_role_arn
+    KARPENTER_NODE_ROLE = module.karpenter.node_iam_role_name
+    KARPENTER_ARN       = module.karpenter.node_iam_role_arn
   }
 }
