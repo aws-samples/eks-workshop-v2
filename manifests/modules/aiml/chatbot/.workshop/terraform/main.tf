853ad0357fc77aa20ef155d7cacc210e0354cd46
Sai Vennam <svennam92@gmail.com>
2024-09-25 20:38:51 -0500

    fix terraform formatting

--- manifests/modules/aiml/chatbot/.workshop/terraform/main.tf
+++ manifests/modules/aiml/chatbot/.workshop/terraform/main.tf
@@ -102,8 +102,8 @@ resource "helm_release" "karpenter" {
   repository_password = data.aws_ecrpublic_authorization_token.token.password
   chart               = "karpenter"
   # renovate: datasource=github-releases depName=aws/karpenter-provider-aws
-  version             = "1.0.2"
-  wait                = true
+  version = "1.0.2"
+  wait    = true
 
   values = [
     <<-EOT
