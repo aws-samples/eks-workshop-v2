1992e708759574dac94024da6557da6f843c047e
Sai Vennam <svennam92@gmail.com>
2024-09-27 14:29:49 -0500

    referrer is ok

--- website/src/theme/DocSidebarItem/Link/index.js
+++ website/src/theme/DocSidebarItem/Link/index.js
@@ -73,7 +73,7 @@ export default function DocSidebarItemLink({
             className="clean-btn menu__link"
             onClick={(e) => {
               e.stopPropagation();
-              window.open(item.customProps.explore, "_blank", "noopener,noreferrer");
+              window.open(item.customProps.explore, "_blank", "noopener");
             }}
           >
             <IconExternalLink />
