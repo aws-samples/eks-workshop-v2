799c2596fab4871090823ef220440644f51662c6
Benjamin Gardiner <69600693+bkgardiner@users.noreply.github.com>
2024-10-01 10:12:47 -0700

    Removed miss placed env vars

--- manifests/modules/aiml/chatbot/.workshop/terraform/outputs.tf
+++ manifests/modules/aiml/chatbot/.workshop/terraform/outputs.tf
@@ -4,7 +4,5 @@ output "environment_variables" {
     AIML_SUBNETS        = "${data.aws_subnets.private.ids[0]},${data.aws_subnets.private.ids[1]},${data.aws_subnets.private.ids[2]}"
     KARPENTER_NODE_ROLE = module.karpenter.node_iam_role_name
     KARPENTER_ARN       = module.karpenter.node_iam_role_arn
-    AIML_DL_TRN_IMAGE   = "public.ecr.aws/neuron/pytorch-training-neuronx:2.1.2-neuronx-py310-sdk2.20.0-ubuntu20.04"
-    AIML_DL_INF_IMAGE   = "public.ecr.aws/neuron/pytorch-inference-neuronx:2.1.2-neuronx-py310-sdk2.20.0-ubuntu20.04"
   }
 }
