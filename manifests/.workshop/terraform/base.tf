0ec32c37f2a409e140d5c74dc2a3c0e864c77e4e
Sai Vennam <svennam92@gmail.com>
2024-10-16 10:00:12 -0400

    precommit checks passing

--- manifests/.workshop/terraform/base.tf
+++ manifests/.workshop/terraform/base.tf
@@ -4,7 +4,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "5.71.0"
+      version = "5.72.0"
     }
     kubernetes = {
       source  = "hashicorp/kubernetes"
