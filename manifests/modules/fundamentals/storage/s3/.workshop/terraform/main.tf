aae29c4d56ee4aadf53760c6296bf2d0838b73b8
Rishiraj Chandra <rishircn@amazon.com>
2024-11-04 16:39:15 -0500

    Making changes to fix PR issues

--- manifests/modules/fundamentals/storage/s3/.workshop/terraform/main.tf
+++ manifests/modules/fundamentals/storage/s3/.workshop/terraform/main.tf
@@ -1,24 +1,24 @@
 # Create S3 bucket
-resource "aws_s3_bucket" "mountpoint-s3" {
+resource "aws_s3_bucket" "mountpoint_s3" {
 
-   bucket_prefix = "${var.addon_context.eks_cluster_id}-mountpoint-s3"
-   force_destroy = true
+  bucket_prefix = "${var.addon_context.eks_cluster_id}-mountpoint-s3"
+  force_destroy = true
 }
 
 # Create S3 CSI Driver IAM Role and associated policy
 module "mountpoint_s3_csi_driver_irsa" {
-  source = "terraform-aws-modules/iam/aws//modules/iam-role-for-service-accounts-eks"
+  source  = "terraform-aws-modules/iam/aws//modules/iam-role-for-service-accounts-eks"
   version = "5.39.1"
 
   # Create prefixes
-  role_name_prefix = "${var.addon_context.eks_cluster_id}-s3-csi-"
+  role_name_prefix   = "${var.addon_context.eks_cluster_id}-s3-csi-"
   policy_name_prefix = "${var.addon_context.eks_cluster_id}-s3-csi-"
 
   # IAM policy to attach to driver
   attach_mountpoint_s3_csi_policy = true
 
-  mountpoint_s3_csi_bucket_arns   = [aws_s3_bucket.mountpoint-s3.arn]
-  mountpoint_s3_csi_path_arns     = ["${aws_s3_bucket.mountpoint-s3.arn}/*"]
+  mountpoint_s3_csi_bucket_arns = [aws_s3_bucket.mountpoint-s3.arn]
+  mountpoint_s3_csi_path_arns   = ["${aws_s3_bucket.mountpoint-s3.arn}/*"]
 
   oidc_providers = {
     main = {
--- manifests/modules/fundamentals/storage/s3/.workshop/terraform/outputs.tf
+++ manifests/modules/fundamentals/storage/s3/.workshop/terraform/outputs.tf
@@ -2,7 +2,7 @@ output "environment_variables" {
   description = "Environment variables to be added to the IDE shell"
   value = {
     S3_CSI_ADDON_ROLE = module.mountpoint_s3_csi_driver_irsa.iam_role_arn
-    BUCKET_NAME = aws_s3_bucket.mountpoint-s3.id
-    EKS_CLUSTER_NAME = var.eks_cluster_id
+    BUCKET_NAME       = aws_s3_bucket.mountpoint-s3.id
+    EKS_CLUSTER_NAME  = var.eks_cluster_id
   }
 }
