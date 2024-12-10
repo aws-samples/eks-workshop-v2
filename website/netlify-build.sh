f18cbc9f262c0af81506e152d35f86b5eed9d992
Niall Thomson <thomson.niall@gmail.com>
2024-12-10 15:46:05 -0700

    Fix netlify preview

--- website/netlify-build.sh
+++ website/netlify-build.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-source ../hack/lib/kubectl-version.sh
+source ./hack/lib/kubectl-version.sh
 
 wget -q https://dl.k8s.io/release/$KUBECTL_VERSION/bin/linux/amd64/kubectl
 chmod +x ./kubectl
