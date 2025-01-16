7340912162db00b7b7f096ba6f1d9a111c13cbe3
Niall Thomson <thomson.niall@gmail.com>
2025-01-16 17:30:46 +0000

    fix: Fix IAM role for S3 Mountpoint lab

--- manifests/modules/fundamentals/storage/s3/.workshop/cleanup.sh
+++ manifests/modules/fundamentals/storage/s3/.workshop/cleanup.sh
@@ -4,17 +4,17 @@
 
 set -e
 
+logmessage "Deleting assets-images folder..."
+
 # Delete local directory of image files
 rm -rf ~/environment/assets-images/
 
-logmessage "Deleting assets-images folder..."
-
 addon_exists=$(aws eks list-addons --cluster-name $EKS_CLUSTER_NAME --query "addons[? @ == 'aws-mountpoint-s3-csi-driver']" --output text)
+
+logmessage "Scaling down assets deployment..."
 
 # Scale down assets
 kubectl scale -n assets --replicas=0 deployment/assets
-
-logmessage "Scaling down assets deployment..."
 
 # Check if the S3 CSI driver addon exists
 if [ ! -z "$addon_exists" ]; then
@@ -26,10 +26,10 @@ if [ ! -z "$addon_exists" ]; then
   aws eks wait addon-deleted --cluster-name $EKS_CLUSTER_NAME --addon-name aws-mountpoint-s3-csi-driver
 fi
 
+logmessage "Deleting PV and PVC that were created..."
+
 # Delete PVC
 kubectl delete pvc s3-claim -n assets --ignore-not-found=true
 
 # Delete PV
 kubectl delete pv s3-pv --ignore-not-found=true
-
-logmessage "Deleting PV and PVC that were created..."
