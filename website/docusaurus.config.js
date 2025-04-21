ad65264ba8903db28c0f6c38bc5db72faadf49a9
Niall Thomson <thomson.niall@gmail.com>
2025-04-21 15:46:53 -0600

    Add module back

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -85,8 +85,7 @@ const config = {
           editUrl:
             "https://github.com/aws-samples/eks-workshop-v2/tree/main/website",
           exclude: [
-            "security/guardduty/runtime-monitoring/reverse-shell.md",
-            "fundamentals/storage/fsx-for-netapp-ontap",
+            "security/guardduty/runtime-monitoring/reverse-shell.md"
           ],
         },
         theme: {
