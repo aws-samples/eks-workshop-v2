54faef4415abe5050abded4764629bd40c49506c
Rishiraj Chandra <rishircn@amazon.com>
2025-04-24 23:52:37 -0400

    Minor changes to FSxL module

--- manifests/modules/fundamentals/storage/fsxl/.workshop/cleanup.sh
+++ manifests/modules/fundamentals/storage/fsxl/.workshop/cleanup.sh
@@ -6,10 +6,6 @@ check=$(helm list -n kube-system | grep aws-fsx-csi-driver || true)
 
 kubectl scale -n assets --replicas=0 deployment/assets
 
-logmessage "Deleting FSX Lustre storage class..."
-
-kubectl delete storageclass fsx-sc --ignore-not-found
-
 if [ ! -z "$check" ]; then
   logmessage "Deleting FSX Lustre CSI driver addon..."
 
@@ -18,5 +14,8 @@ fi
 
 # Delete PVC
 kubectl delete pvc fsx-claim -n assets --ignore-not-found=true
+
+# Delete PV
+kubectl delete pv fsx-pv --ignore-not-found=true
 
 logmessage "Deleting PV and PVC that were created..."
