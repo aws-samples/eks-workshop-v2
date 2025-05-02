35ae7dae2afe5bed08c5516bed73cd1b4eeaf3a1
Sai Vennam <svennam92@gmail.com>
2025-05-02 12:05:49 +0800

    further terraform val fix

--- manifests/modules/troubleshooting/dns/.workshop/terraform/outputs.tf
+++ manifests/modules/troubleshooting/dns/.workshop/terraform/outputs.tf
@@ -1,8 +1,3 @@
-output "account_id" {
-  value       = local.account_id
-  description = "account id env variable"
-}
-
 output "environment_variables" {
   description = "Environment variables to be added to the IDE shell"
   value = {
