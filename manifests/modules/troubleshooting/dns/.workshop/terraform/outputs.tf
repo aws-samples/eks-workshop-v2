f2cb8e31c586ec6a93f4ba16e8dcefe2770c9ae9
Sai Vennam <svennam92@gmail.com>
2025-05-02 12:02:46 +0800

    fix terraform validation

--- manifests/modules/troubleshooting/dns/.workshop/terraform/outputs.tf
+++ manifests/modules/troubleshooting/dns/.workshop/terraform/outputs.tf
@@ -5,12 +5,8 @@ output "account_id" {
 
 output "environment_variables" {
   description = "Environment variables to be added to the IDE shell"
-  value = merge({
-    VPC_ID           = data.aws_vpc.selected.id,
+  value = {
     EKS_CLUSTER_NAME = var.addon_context.eks_cluster_id
-    }, {
-    for index, id in data.aws_subnets.public.ids : "PUBLIC_SUBNET_${index + 1}" => id
-    }
-  )
+  }
 }
 
