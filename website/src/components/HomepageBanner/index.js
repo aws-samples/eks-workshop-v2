e3b043c08c8343784b1bd00be3bad1332c8205d5
Sai Vennam <svennam92@gmail.com>
2025-05-23 10:54:44 -0500

    chore: Update homepage promo link (#1448)

--- website/src/components/HomepageBanner/index.js
+++ website/src/components/HomepageBanner/index.js
@@ -7,12 +7,12 @@ const HomepageBanner = () => {
   return (
     <div className={styles.banner}>
       <span className={styles.bannerText}>
-      Simplify Kubernetes with <b>Amazon EKS Auto Mode</b>:{" "}
+        <b>Virtual Workshop Experience</b> — Get Hands On with Amazon EKS:{" "}
         <a 
-          href="https://aws-experience.com/emea/smb/events/series/simplifying-kubernetes-operations-with-amazon-eks-auto-mode?trk=e3d0398c-e0e9-4665-af82-a2e8124a6db8&sc_channel=el"
+          href="https://aws-experience.com/emea/smb/events/series/get-hands-on-with-amazon-eks?trk=d42ffbaa-d60a-4513-8a79-0bf36b9f33ce&sc_channel=el"
           target="_blank"
         >
-          Webinar <FontAwesomeIcon
+          Event Series <FontAwesomeIcon
             icon={faArrowUpRightFromSquare}
             className={styles.linkIcon}
           />
