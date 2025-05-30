56756eecf736abb62fb1f6e74991face5e11ae13
tzahimizrahi <5658093+tzahimizrahi@users.noreply.github.com>
2025-05-30 21:36:19 +0300

    update: Added ExternalDNS to Ingress lab (#1413)
    Co-authored-by: Tzahi Mizrahi <tzahim@amazon.com>
    Co-authored-by: Niall Thomson <thomson.niall@gmail.com>

--- manifests/modules/exposing/ingress/.workshop/terraform/main.tf
+++ manifests/modules/exposing/ingress/.workshop/terraform/main.tf
@@ -1,3 +1,36 @@
+data "aws_vpc" "this" {
+  tags = {
+    created-by = "eks-workshop-v2"
+    env        = var.addon_context.eks_cluster_id
+  }
+}
+
+data "external" "vs_code_vpc_id" {
+  program = ["bash", "-c", <<EOF
+    TOKEN=$(curl -s -X PUT "http://169.254.169.254/latest/api/token" -H "X-aws-ec2-metadata-token-ttl-seconds: 21600")
+    MAC=$(curl -s -H "X-aws-ec2-metadata-token: $TOKEN" http://169.254.169.254/latest/meta-data/mac)
+    VPC_ID=$(curl -s -H "X-aws-ec2-metadata-token: $TOKEN" http://169.254.169.254/latest/meta-data/network/interfaces/macs/$MAC/vpc-id)
+    echo "{\"vpc_id\": \"$VPC_ID\"}"
+EOF
+  ]
+}
+
+resource "aws_route53_zone" "private_zone" {
+  name    = "retailstore.com"
+  comment = "Private hosted zone for EKS Workshop use"
+  vpc {
+    vpc_id = data.aws_vpc.this.id
+  }
+  vpc {
+    vpc_id = data.external.vs_code_vpc_id.result.vpc_id
+  }
+
+  tags = {
+    created-by = "eks-workshop-v2"
+    env        = var.addon_context.eks_cluster_id
+  }
+}
+
 module "eks_blueprints_addons" {
   source  = "aws-ia/eks-blueprints-addons/aws"
   version = "1.21.0"
@@ -6,6 +39,14 @@ module "eks_blueprints_addons" {
   cluster_endpoint  = var.addon_context.aws_eks_cluster_endpoint
   cluster_version   = var.eks_cluster_version
   oidc_provider_arn = var.addon_context.eks_oidc_provider_arn
+
+  enable_external_dns            = true
+  external_dns_route53_zone_arns = [aws_route53_zone.private_zone.arn]
+  external_dns = {
+    create_role = true
+    role_name   = "${var.addon_context.eks_cluster_id}-external-dns"
+    policy_name = "${var.addon_context.eks_cluster_id}-external-dns"
+  }
 
   enable_aws_load_balancer_controller = true
   aws_load_balancer_controller = {
