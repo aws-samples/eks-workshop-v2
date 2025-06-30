b30eecff2d58ee2162e39e747718fa45be0f33c1
Niall Thomson <thomson.niall@gmail.com>
2025-06-30 14:12:05 -0600

    chore: Bump AWS TF provider (#1489)

--- manifests/.workshop/terraform/base.tf
+++ manifests/.workshop/terraform/base.tf
@@ -4,7 +4,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "5.99.1"
+      version = "5.100.0"
     }
     kubernetes = {
       source  = "hashicorp/kubernetes"
