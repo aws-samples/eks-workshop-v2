7917580eb4f5c138c8add62e6c5021f851a4d93c
Niall Thomson <thomson.niall@gmail.com>
2025-07-10 23:37:35 +0000

    chore: Added local IDE for testing

--- lab/scripts/setup.sh
+++ lab/scripts/setup.sh
@@ -76,3 +76,30 @@ cat << EOT > /home/ec2-user/.banner-text
                       Hands-on labs for Amazon Elastic Kubernetes Service
 
 EOT
+
+mkdir -p ~/.local/share/code-server/User
+touch ~/.local/share/code-server/User/settings.json
+tee ~/.local/share/code-server/User/settings.json <<EOF
+{
+  "extensions.autoUpdate": false,
+  "extensions.autoCheckUpdates": false,
+  "security.workspace.trust.enabled": false,
+  "task.allowAutomaticTasks": "on",
+  "telemetry.telemetryLevel": "off",
+  "workbench.startupEditor": "terminal"
+}
+EOF
+
+mkdir -p ~/environment/.vscode
+tee ~/environment/.vscode/settings.json <<EOF
+{
+  "files.exclude": {
+    "**/.*": true
+  }
+}
+EOF
+
+echo '{ "query": { "folder": "/home/ec2-user/environment" } }' > ~/.local/share/code-server/coder.json
+
+code-server --install-extension redhat.vscode-yaml --force || true
+code-server --install-extension ms-kubernetes-tools.vscode-kubernetes-tools --force || true
