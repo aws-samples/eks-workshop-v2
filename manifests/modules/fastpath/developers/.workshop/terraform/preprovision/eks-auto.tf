f9e055821f18b331f02c66bb80357f4e9eb90e93
Math Bruneau <brunemat@amazon.com>
2025-10-21 14:44:15 -0400

    Fix for use 2.17 provider

--- manifests/modules/fastpath/developers/.workshop/terraform/preprovision/eks-auto.tf
+++ manifests/modules/fastpath/developers/.workshop/terraform/preprovision/eks-auto.tf
@@ -16,8 +16,19 @@ data "aws_availability_zones" "available" {
 data "aws_region" "current" {}
 
 # Helm provider configuration for EKS
+terraform {
+  required_version = ">= 1.3"
+
+  required_providers {
+    helm = {
+      source  = "hashicorp/helm"
+      version = "2.17.0"
+    }
+  }
+}
+
 provider "helm" {
-    kubernetes = {
+    kubernetes {
       host = data.aws_eks_cluster.eks_cluster.endpoint
       cluster_ca_certificate = base64decode(data.aws_eks_cluster.eks_cluster.certificate_authority[0].data)
       token = data.aws_eks_cluster_auth.this.token
