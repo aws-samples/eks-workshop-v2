5ee652dedb24444608118177b2a85b39b7e80307
Math Bruneau <brunemat@amazon.com>
2025-10-21 10:15:33 -0400

    Ensure completeness

--- cluster/terraform-auto/variables.tf
+++ cluster/terraform-auto/variables.tf
@@ -3,9 +3,3 @@ variable "auto_cluster_name" {
   type        = string
   default     = "eks-workshop-auto"
 }
-
-variable "cluster_version" {
-  description = "EKS cluster version"
-  type        = string
-  default     = "1.33"
-}
