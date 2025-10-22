f81050716f8e4dd42adb470f10cd0c278b1ffba1
Math Bruneau <brunemat@amazon.com>
2025-10-21 22:25:45 -0400

    Moved to eksctl

--- manifests/modules/fastpath/developers/.workshop/terraform/preprovision/eks-auto.tf
+++ manifests/modules/fastpath/developers/.workshop/terraform/preprovision/eks-auto.tf
@@ -34,12 +34,3 @@ provider "helm" {
       token = data.aws_eks_cluster_auth.this.token
     }
 }
-resource "aws_eks_access_policy_association" "teamstack" {
-      cluster_name = data.aws_eks_cluster.eks_cluster.name
-      principal_arn = data.aws_caller_identity.current.user_id
-      policy_arn = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSAdminPolicy"
-
-      access_scope {
-        type = "cluster"
-      }
-}
