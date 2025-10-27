30311fdca1f8cebd05fddea6321d34d1b2480f1a
Math Bruneau <brunemat@amazon.com>
2025-10-27 16:09:49 -0400

    Added preprovision for eks-pod-identity

---
+++ manifests/modules/fastpaths/operators/eks-pod-identity/.workshop/terraform/preprovision/vars.tf
@@ -0,0 +1,18 @@
+# tflint-ignore: terraform_unused_declarations
+variable "eks_cluster_id" {
+  description = "EKS cluster name"
+  type        = string
+}
+
+# tflint-ignore: terraform_unused_declarations
+variable "eks_cluster_auto_id" {
+  description = "EKS Auto Mode cluster name"
+  type        = string
+  default     = "eks-workshop-auto"
+}
+
+# tflint-ignore: terraform_unused_declarations
+variable "tags" {
+  description = "Tags to apply to AWS resources"
+  type        = any
+}
