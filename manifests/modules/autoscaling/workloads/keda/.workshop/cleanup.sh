d5c1556fd5261293abe30eaed76230a266c1a132
Niall Thomson <thomson.niall@gmail.com>
2025-10-28 15:13:15 -0600

    Update cleanup.sh

--- manifests/modules/autoscaling/workloads/keda/.workshop/cleanup.sh
+++ manifests/modules/autoscaling/workloads/keda/.workshop/cleanup.sh
@@ -3,7 +3,7 @@
 set -e
 
 kubectl delete pod load-generator --ignore-not-found
-kubectl delete scaledobject ui-hpa -n ui --ignore-not-found
+delete-all-if-crd-exists scaledobjects.keda.sh
 kubectl delete ingress ui -n ui --ignore-not-found
 
 uninstall-helm-chart keda keda
