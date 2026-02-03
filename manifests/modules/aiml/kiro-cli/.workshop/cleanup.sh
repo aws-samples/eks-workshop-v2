2df30073d1f898ce124273742250e85bf791dd6d
Parth Pandit <parth.pandit@hotmail.com>
2026-02-02 21:34:51 -0600

    Changes to replace Q CLI with Kiro CLI

--- manifests/modules/aiml/kiro-cli/.workshop/cleanup.sh
+++ manifests/modules/aiml/kiro-cli/.workshop/cleanup.sh
@@ -4,4 +4,4 @@ set -e
 
 logmessage "Deleting failing pod..."
 
-kubectl delete -f /eks-workshop/manifests/modules/aiml/q-cli/troubleshoot/failing-pod.yaml --ignore-not-found
+kubectl delete -f /eks-workshop/manifests/modules/aiml/kiro-cli/troubleshoot/failing-pod.yaml --ignore-not-found
