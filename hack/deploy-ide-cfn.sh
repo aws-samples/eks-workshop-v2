d9e7a7a9a0685fe7206d1e29d3017ff691e37eb8
Math Bruneau <brunemat@amazon.com>
2026-03-25 12:03:49 -0400

    fix: pass repository params to CFN template and fix REPOSITORY_REF typo

--- hack/deploy-ide-cfn.sh
+++ hack/deploy-ide-cfn.sh
@@ -10,5 +10,13 @@ outfile=$(mktemp)
 
 bash $SCRIPT_DIR/build-ide-cfn.sh $outfile
 
+REPOSITORY_OWNER=${REPOSITORY_OWNER:-"aws-samples"}
+REPOSITORY_NAME=${REPOSITORY_NAME:-"eks-workshop-v2"}
+REPOSITORY_REF=${REPOSITORY_REF:-"main"}
+
 aws cloudformation deploy --stack-name "$EKS_CLUSTER_NAME-ide" \
-  --capabilities CAPABILITY_NAMED_IAM --disable-rollback --template-file $outfile
+  --capabilities CAPABILITY_NAMED_IAM --disable-rollback --template-file $outfile \
+  --parameter-overrides \
+    RepositoryOwner="$REPOSITORY_OWNER" \
+    RepositoryName="$REPOSITORY_NAME" \
+    RepositoryRef="$REPOSITORY_REF"
